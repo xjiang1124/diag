6734a0d0374abfa9a8bf990f8b9a2b4137ea257e
Xin Guo <xin.guo@pensando.io>
2017-07-12 23:08:17 -0700

    Go redis example

---
+++ example/goRedis/redisTest.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+    "github.com/go-redis/redis"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+
+	err = client.Set("key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := client.Get("key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+}
+
