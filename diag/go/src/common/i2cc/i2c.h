c7bca4177c872cc73be851472c9ad48f9a9db750
Xin Guo <xin.guo@pensando.io>
2017-08-10 23:20:55 -0700

    go calling C

---
+++ diag/go/src/common/i2cc/i2c.h
@@ -0,0 +1,9 @@
+#ifndef I2C_H
+#define I2C_H
+
+#include "../../../include/cType.h"
+
+int I2cRead(int devAddr, int regAddr, int* value, int numBytes);
+int I2cWrite(int devAddr, int regAddr, int *value, int numBytes);
+
+#endif //#define I2C_H
