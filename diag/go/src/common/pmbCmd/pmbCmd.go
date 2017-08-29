bb5b1cb2757a99715fb5bc8a6be3d73c329d388a
Xin Guo <xin.guo@pensando.io>
2017-08-29 15:20:36 -0700

    Initial version of pmbCmd.go

---
+++ diag/go/src/common/pmbCmd/pmbCmd.go
@@ -0,0 +1,23 @@
+// pmb.go specifies PMBus commands implementations
+package pmbCmd
+
+func readByte(devAddr uint32, regAddr uint32) value uint32 {
+    return 0
+}
+
+func writeByte(devAddr uint32, regAddr uint32, value uint32) value uint32 {
+    return 0
+}
+
+func readWord(devAddr uint32, regAddr uint32) value uint32 {
+    return 0
+}
+
+func writeWord(devAddr uint32, regAddr uint32, value uint32) value uint32 {
+    return 0
+}
+
+func sendByte(devAddr uint32, regAddr uint32) value uint32 {
+    return 0
+}
+
