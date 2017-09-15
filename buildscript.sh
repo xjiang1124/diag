da8b3841989030deaf6e387c2477b86f0ac9e461
Xin Guo <xin.guo@pensando.io>
2017-09-15 12:07:15 -0700

    buildscript update

--- buildscript.sh
+++ buildscript.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+top_dir=$PWD
+export GOPATH=${top_dir}/diag/app
+export GOBIN=${top_dir}/diag/app/bin
+export LD_LIBRARY_PATH=${top_dir}/diag/app/pkg/linux_amd64/common/
+export LIBRARY_PATH=${top_dir}/diag/app/pkg/linux_amd64/common/
 cd build
 make c_build
 make build
