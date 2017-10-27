afa4b01301bfaaa5717879bba3149e369e982d40
Xin Guo <xin.guo@pensando.io>
2017-10-26 17:31:30 -0700

    Rearrange build to be per folder

---
+++ build/common.mk
@@ -0,0 +1,29 @@
+SHELL := /bin/bash
+
+#BUILD_DIR = $(shell pwd)
+
+GO_CMD=go
+GO_BUILD=$(GO_CMD) build
+GO_INSTALL=$(GO_CMD) install
+GO_TEST=$(GO_CMD) test
+#GOPATH=$(TOP_DIR)/diag/go
+#CGO_LDFLAGS=-L$(GOPATH)/pkg/linux_amd64/common
+
+# These will be provided to the target
+VERSION := 1.0.0
+BUILD := `git rev-parse HEAD`
+
+# Use linker flags to provide version/build settings to the target
+LDFLAGS=-ldflags "-X=main.Version=$(VERSION) -X=main.Build=$(BUILD)"
+
+ARCH ?= amd64
+
+CC_ARM_64=aarch64-linux-gnu-gcc
+CC_X86=gcc
+
+ifeq (${ARCH}, amd64)
+    CMPL=$(CC_X86)
+else
+    CMPL=$(CC_ARM_64)
+endif
+											
