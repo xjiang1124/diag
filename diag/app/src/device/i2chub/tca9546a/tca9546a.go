57a292014cc4751f0ecf851ca5d99c9d605fdbdc
Xin Guo <xin.guo@pensando.io>
2018-01-17 18:26:38 -0800

    I2C hub code

---
+++ diag/app/src/device/i2chub/tca9546a/tca9546a.go
@@ -0,0 +1,44 @@
+package tca9546a
+
+import (
+    "common/errType"
+
+    "protocol/smbus"
+)
+
+const (
+)
+
+/*
+    Enble single channel 0-3
+    All other channels will be disabled
+ */
+func EnableChan(devName string, channel byte) (err int) {
+    var data byte
+
+    if channel > 3 {
+        err = errType.INVALID_PARAM
+        return
+    }
+
+    data = 1 << channel
+    err = smbus.SendByte(devName, data)
+    return
+}
+
+/*
+    Disable all channels
+ */
+func DisableAllChan(devName string) (err int) {
+    err = smbus.SendByte(devName, 0)
+    return
+}
+
+/*
+    Disable all channels
+ */
+func ReadStatus(devName string) (data byte, err int) {
+    data, err = smbus.ReceiveByte(devName)
+    return
+}
+
