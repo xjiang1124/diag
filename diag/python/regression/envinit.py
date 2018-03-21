2ed4cca984eeba79a0693cc5859331c8120121ce
Xin Guo <xin.guo@pensando.io>
2018-03-20 17:52:00 -0700

    envinit update to include USB device

--- diag/python/regression/envinit.py
+++ diag/python/regression/envinit.py
@@ -22,6 +22,12 @@ if i == 0:
 child.sendline("sudo rmmod ftdi_sio")
 child.expect("\$ ")
 
+child.sendline("sudo chmod ugo+rw /dev/bus/usb/001/001")
+child.expect("\$ ")
+
+child.sendline("sudo chmod ugo+rw /dev/bus/usb/001/002")
+child.expect("\$ ")
+
 child.close()
 
 print "envinit Done"
