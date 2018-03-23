cf57ae72fb3752011eb1a67a23e564008eb7a75d
Xin Guo <xin.guo@pensando.io>
2018-03-23 16:40:16 -0700

    Fix arm64 compile error and add common/platform path

--- build/path.mk
+++ build/path.mk
@@ -1,4 +1,4 @@
-SRC_DIR=				\
+COMMON_SRC_DIR=				\
 	diagmgr 			\
 	dsp/pmbus   		\
 	dsp/qsfp    		\
@@ -10,23 +10,33 @@ SRC_DIR=				\
 	util/smbutil 		\
 	util/devmgr
 
-#C_SRC_DIR=i2c
+COMMON_C_LIB_DIR=		\
+	lib/i2c				\
+	lib/i2csim			\
+	lib/cpld
 
-C_LIB_DIR=		\
-	i2c		\
-	i2csim		\
-	cpld
+COMMON_C_SRC_DIR=
 
-C_SRC_DIR=		\
-	jtag		\
-	cpld		\
-	mdio		\
-	spi
-
-GO_TEST_DIR=	\
+GO_TEST_DIR=						\
 	device/powermodule/tps53659		\
 	device/powermodule/tps549a20	\
 	device/tempsensor/tmp422		\
 	device/rtc/pcf85263a			\
 	common/dmutex					\
 	util/periutil					
+
+AMD64_SRC_DIR=
+AMD64_C_LIB_DIR=
+
+AMD64_C_SRC_DIR=		\
+	util/jtag			\
+	util/cpld			\
+	util/mdio			\
+	util/spi
+
+ARM64_SRC_DIR=
+ARM64_C_LIB_DIR=
+ARM64_C_SRC_DIR=
+
+
+
