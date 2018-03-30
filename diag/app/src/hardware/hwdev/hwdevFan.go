83cb9c14efdcdfc1f2ab00ef57760c0d6dda869a
Xin Guo <xin.guo@pensando.io>
2018-03-30 10:29:53 -0700

    Offload fan control to hwdev

---
+++ diag/app/src/hardware/hwdev/hwdevFan.go
@@ -0,0 +1,70 @@
+package hwdev
+
+import (
+    "strconv"
+
+    "common/cli"
+    "common/dmutex"
+    "common/errType"
+
+    "device/fanctrl/adt7462"
+
+    "hardware/hwinfo"
+    "hardware/i2cinfo"
+
+)
+
+func FanSpeed(devName string, pct int, mask uint64) (err int) {
+    var i uint64
+    var i2cif i2cinfo.I2cInfo
+
+    i2cif, err = i2cinfo.GetI2cInfo(devName)
+    if err != errType.SUCCESS {
+        return
+    }
+
+    lockName := "i2c-"+strconv.Itoa(int(i2cif.Bus))
+    err = dmutex.Lock(lockName)
+    if err != errType.SUCCESS {
+        return
+    }
+    defer dmutex.Unlock(lockName)
+
+    hwinfo.EnableHubChannelExclusive(devName)
+    if i2cif.Comp == "ADT7462" {
+        for i = 0; i < hwinfo.MAX_NUM_FAN; i++ {
+            if (mask & (1 << i)) != 0 {
+                adt7462.SetFanSpeed(devName, uint64(i), uint64(pct))
+            }
+        }
+    } else {
+        cli.Println("e", "Unsupported component: ", i2cif.Comp)
+        err = errType.INVALID_PARAM
+    }
+    return
+}
+
+func FanSetup(devName string) (err int) {
+    var i2cif i2cinfo.I2cInfo
+
+    i2cif, err = i2cinfo.GetI2cInfo(devName)
+    if err != errType.SUCCESS {
+        return
+    }
+
+    lockName := "i2c-"+strconv.Itoa(int(i2cif.Bus))
+    err = dmutex.Lock(lockName)
+    if err != errType.SUCCESS {
+        return
+    }
+    defer dmutex.Unlock(lockName)
+
+    hwinfo.EnableHubChannelExclusive(devName)
+    if i2cif.Comp == "ADT7462" {
+        adt7462.Setup(devName)
+    } else {
+        cli.Println("e", "Unsupported component: ", i2cif.Comp)
+        err = errType.INVALID_PARAM
+    }
+    return
+}
--- diag/app/src/util/devmgr/devmgr.go
+++ diag/app/src/util/devmgr/devmgr.go
@@ -4,197 +4,19 @@ import (
     //"fmt"
     "flag"
     "os"
-    "sort"
     "strings"
 
     "common/cli"
     "common/errType"
     "config"
     "hardware/i2cinfo"
-    "hardware/hwinfo"
     "hardware/hwdev"
-    "device/powermodule/tps53659"
-    "device/powermodule/tps549a20"
-    "device/fanctrl/adt7462"
 )
 
 func init() {
     procNameTemp := strings.Split(os.Args[0], "/")
     procName := procNameTemp[len(procNameTemp)-1]
     cli.Init("log_"+procName+".txt", config.OutputMode)
-}
-
-func devInfo(devName string, uut string) {
-    for _, i2cInfo := range(i2cinfo.I2cTbl) {
-        if devName != i2cInfo.Name {
-            continue
-        }
-        if i2cInfo.Comp == "TPS53659" {
-            tps53659.Info(devName)
-            return
-        }
-    }
-    cli.Println("e", "Unsupported device: ", devName)
-}
-
-func dispStatus(devName string) (err int){
-    if devName == "ALL" {
-        // golang range(map) sequence is random
-        // Sorted output
-        keys := make([]string, 0)
-        for k, _ := range(hwinfo.DispStaList) {
-            keys = append(keys, k)
-        }
-        sort.Strings(keys)
-        for _, dev := range(keys) {
-            dispFunc := hwinfo.DispStaList[dev]
-            hwinfo.EnableHubChannelExclusive(dev)
-            dispFunc(dev)
-        }
-    } else {
-        hwinfo.EnableHubChannelExclusive(devName)
-        dispFunc, ok := hwinfo.DispStaList[devName]
-        if ok == false {
-            cli.Println("f", "Invalde device: ", devName)
-            err = errType.INVALID_PARAM
-            return
-        }
-        dispFunc(devName)
-    }
-    return
-}
-
-func dispStatusUut(devName string, uutName string) (err int){
-    if devName == "ALL" {
-        cli.Println("e", "\"ALL\" does not support by UUT!")
-        err = errType.INVALID_PARAM
-        return
-    }
-
-    err = hwinfo.EnableHubChannelUut(uutName)
-    if err != errType.SUCCESS {
-        return err
-    }
-
-    hwinfo.SwitchHwInfo(uutName)
-    i2cinfo.SwitchI2cTbl(uutName)
-    dispFunc, ok := hwinfo.DispStaList[devName]
-    if ok == false {
-        cli.Println("f", "Invalde device: ", devName)
-        err = errType.INVALID_PARAM
-        return
-    }
-    dispFunc(devName)
-    i2cinfo.SwitchI2cTbl("UUT_NONE")
-    hwinfo.SwitchHwInfo("UUT_NONE")
-
-    return
-}
-
-func margin(devName string, pct int) (err int){
-    for _, i2cInfo := range(i2cinfo.I2cTbl) {
-        if devName != i2cInfo.Name {
-            continue
-        }
-        if i2cInfo.Comp == "TPS53659" {
-            tps53659.SetVMargin(devName, pct)
-            return
-        } else if i2cInfo.Comp == "TPS549A20" {
-           tps549a20.SetVMargin(devName, pct)
-           return
-        }
-    }
-    cli.Println("e", "Unsupported device: ", devName)
-    err = errType.INVALID_PARAM
-    return
-}
-
-func marginUut(devName string, pct int, uutName string) (err int) {
-    err = hwinfo.EnableHubChannelUut(uutName)
-    if err != errType.SUCCESS {
-        return err
-    }
-
-    i2cinfo.SwitchI2cTbl(uutName)
-    err = margin(devName, pct)
-    i2cinfo.SwitchI2cTbl("UUT_NONE")
-    return
-}
-
-func program (devName string, fileName string, verbose bool) (err int) {
-    for _, i2cInfo := range(i2cinfo.CurI2cTbl) {
-        if devName != i2cInfo.Name {
-            continue
-        }
-        if i2cInfo.Comp == "TPS53659" {
-            tps53659.ProgramVerifyNvm(devName, fileName, "PROGRAM", verbose)
-            return
-        }
-    }
-    cli.Println("e", "Unsupported device: ", devName)
-    err = errType.INVALID_PARAM
-    return
-}
-
-func programUut (devName string, fileName string, verbose bool, uutName string) (err int) {
-    err = hwinfo.EnableHubChannelUut(uutName)
-    if err != errType.SUCCESS {
-        return err
-    }
-
-    i2cinfo.SwitchI2cTbl(uutName)
-    err = program(devName, fileName, verbose)
-    i2cinfo.SwitchI2cTbl("UUT_NONE")
-    return
-}
-
-func verify (devName string, fileName string, verbose bool) (err int) {
-    for _, i2cInfo := range(i2cinfo.CurI2cTbl) {
-        if devName != i2cInfo.Name {
-            continue
-        }
-        if i2cInfo.Comp == "TPS53659" {
-            tps53659.ProgramVerifyNvm(devName, fileName, "VERIFY", verbose)
-            return
-        }
-    }
-    cli.Println("e", "Unsupported device: ", devName)
-    err = errType.INVALID_PARAM
-    return
-}
-
-func verifyUut (devName string, fileName string, verbose bool, uutName string) (err int) {
-    err = hwinfo.EnableHubChannelUut(uutName)
-    if err != errType.SUCCESS {
-        return err
-    }
-
-    i2cinfo.SwitchI2cTbl(uutName)
-    err = verify(devName, fileName, verbose)
-    i2cinfo.SwitchI2cTbl("UUT_NONE")
-    return
-}
-
-func fanSpeed(devName string, pct int, mask uint64) (err int) {
-    var i uint64
-
-    hwinfo.EnableHubChannelExclusive(devName)
-    for _, i2cInfo := range(i2cinfo.I2cTbl) {
-        if devName != i2cInfo.Name {
-            continue
-        }
-        if i2cInfo.Comp == "ADT7462" {
-            for i = 0; i < hwinfo.MAX_NUM_FAN; i++ {
-                if (mask & (1 << i)) != 0 {
-                    adt7462.SetFanSpeed(devName, uint64(i), uint64(pct))
-                }
-            }
-            return
-        }
-    }
-    cli.Println("e", "Unsupported device: ", devName)
-    err = errType.INVALID_PARAM
-    return
 }
 
 func myUsage() {
@@ -255,18 +77,17 @@ func main() {
     }
 
     if *infoPtr == true {
-        devInfo(devName, uut)
+        hwdev.DevInfo(devName, uut)
         return
     }
 
     if *speedPtr == true {
-        fanSpeed(devName, pct, mask)
+        hwdev.FanSpeed(devName, pct, mask)
         return
     }
 
     if *faninitPtr == true {
-        hwinfo.EnableHubChannelExclusive(devName)
-        adt7462.Setup(devName)
+        hwdev.FanSetup(devName)
         return
     }
 
