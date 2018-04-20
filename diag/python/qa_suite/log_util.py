65b8183f8b0f2ac707dbece0872e73d10b5e44b3
Xiaodong Hu <xiaodhu@pensando.io>
2018-04-20 10:59:51 -0700

    capture stdout key words and generate log file

---
+++ diag/python/qa_suite/log_util.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import sys
+
+file_name = sys.argv[1]
+pass_signature = "TEST PASSED!"
+fail_signature = "TEST FAILED!"
+pass_cnt = 0
+fail_cnt = 0
+
+f = open(file_name, "w")
+
+for line in sys.stdin:
+    if pass_signature in line:
+        pass_cnt += 1
+    elif fail_signature in line:
+        fail_cnt += 1
+        print(line)
+    f.write(line)
+
+print("Passed {} tests, failed {} tests".format(pass_cnt, fail_cnt))
+
+f.close()
