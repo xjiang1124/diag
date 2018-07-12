b4bf8cfef0885554faa9cb18d89e0347ebcae1c1
Xin Guo <xin.guo@pensando.io>
2018-07-12 14:56:38 -0700

    naples100 CPLD initial check in

---
+++ diag/app/src/device/cpld/naples100Cpld/naples100CpldFunc.go
@@ -0,0 +1,11 @@
+package naples100Cpld
+
+func ReadReg(addr byte) (data byte, err int) {
+    data = 1
+    return
+}
+
+func WriteReg(addr byte, data byte) (err int) {
+    return
+}
+
---
+++ diag/app/src/device/cpld/naples100Cpld/naples100CpldReg.go
@@ -0,0 +1,28 @@
+package naples100Cpld
+
+const (
+    REV                = 0x00
+    CTRL               = 0x01
+    CTRL_STS           = 0x02
+    INTR_STS           = 0x03
+    LED_CTRL           = 0x04
+    LED_RATE           = 0x05
+    ESW_MDIO_CTRL_LOW  = 0x06
+    ESW_MDIO_CTRL_HIGH = 0x07
+    ESW_MDIO_DATA_LOW  = 0x08
+    ESW_MDIO_DATA_HIGH = 0x09
+    SLOT_ID            = 0x0A
+    SMB_WR_SPI_RD_0    = 0x0B
+    SMB_WR_SPI_RD_1    = 0x0C
+    SPI_WR_SMB_RD_0    = 0x0D
+    SPI_WR_SMB_RD_1    = 0x0E
+    ASIC_CTRL_0        = 0x10
+    ASIC_CTRL_1        = 0x11
+    ASIC_CTRL_2        = 0x12
+    ASIC_GPIO_0        = 0x13
+    ASIC_GPIO_1        = 0x14
+    ASIC_PSST          = 0x20
+    POWER_COST         = 0x30
+    ID                 = 0x80
+)
+
