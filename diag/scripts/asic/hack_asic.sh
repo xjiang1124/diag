0a96047b4f948e9a65aab1b780e395ca31c057ce
Xin Guo <xin.guo@pensando.io>
2018-09-25 16:47:45 -0700

    Update hack_asic scirpt with cp -f

--- diag/scripts/asic/hack_asic.sh
+++ diag/scripts/asic/hack_asic.sh
@@ -1,10 +1,10 @@
 chmod 775 $ASIC_LIB_BUNDLE/depend_libs/lib64/libtcl8.5.so .
 
 cd $ASIC_LIB_BUNDLE/asic_lib/
-cp $ASIC_LIB_BUNDLE/depend_libs/lib64/libJudy.so.1 .
-cp $ASIC_LIB_BUNDLE/depend_libs/lib64/libtcl8.5.so .
-cp $ASIC_LIB_BUNDLE/depend_libs/lib64/libgmpxx.so.4 .
-cp $ASIC_LIB_BUNDLE/depend_libs/lib64/libcrypto.so.10 .
+cp -f $ASIC_LIB_BUNDLE/depend_libs/lib64/libJudy.so.1 .
+cp -f $ASIC_LIB_BUNDLE/depend_libs/lib64/libtcl8.5.so .
+cp -f $ASIC_LIB_BUNDLE/depend_libs/lib64/libgmpxx.so.4 .
+cp -f $ASIC_LIB_BUNDLE/depend_libs/lib64/libcrypto.so.10 .
 
 cd $ASIC_LIB_BUNDLE/asic_src/ip/cosim/tclsh/
 head -n -5 .tclrc.diag > .tclrc.diag.new
