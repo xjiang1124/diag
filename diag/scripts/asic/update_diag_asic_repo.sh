e2fcdad03de78be75239a06e3706110e39b223cf
Xin Guo <xin.guo@pensando.io>
2018-09-26 13:46:55 -0700

    update update_diag_asic_repo.sh

--- diag/scripts/asic/update_diag_asic_repo.sh
+++ diag/scripts/asic/update_diag_asic_repo.sh
@@ -5,12 +5,11 @@
 DIAG_REPO="/vol/hw/diag/diag_repo/asic/amd64"
 ASIC_REPO="/home/xguo2/workspace/asic/"
 ASIC_GEN="/vol/dump/xguo2/workspace/asic"
-cd $DIAG_REPO
-#cp -rf $ASIC_GEN/ip/cosim/diag/nic/* .
+cp -rf $ASIC_GEN/ip/cosim/diag/nic/* .
 cd $ASIC_REPO
 #gitVer=$(git rev-parse --verify HEAD)
 gitVer=$(git log --name-status HEAD^..HEAD > $DIAG_REPO/asic_version.txt.temp)
 echo -e "\n-----------------\n" >> $DIAG_REPO/asic_version.txt.temp
 mv $DIAG_REPO/asic_version.txt $DIAG_REPO/asic_version.txt.temp1
-cat $DIAG_REPO/asic_version.txt.temp $DIAG_REPO/asic_version.txt.temp > cat $DIAG_REPO/asic_version.txt
-cat cat $DIAG_REPO/asic_version.txt
+cat $DIAG_REPO/asic_version.txt.temp $DIAG_REPO/asic_version.txt.temp > $DIAG_REPO/asic_version.txt
+cat $DIAG_REPO/asic_version.txt
