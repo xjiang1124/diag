09dec82b1944692d33e472c76bf0ca007a888c9b
Xin Guo <xin.guo@pensando.io>
2018-10-30 18:17:22 -0700

    Script to update nic mac to permenmant ones

--- diag/python/regression/nic_con.py
+++ diag/python/regression/nic_con.py
@@ -13,15 +13,15 @@ class nic_con:
     def __init__(self):
         self.usr = "root"
         self.pwd = "pen123"
-        self.fmt_con_cmd = "picocom -b {} -f h /dev/ttyS1"
+        self.fmt_con_cmd = "picocom -b {} -f h /dev/ttyS1\n"
         self.fmt_change_rate = "stty speed {}"
 
     def uart_session_start(self, session, baud=115200):
         cmd = self.fmt_con_cmd.format(baud)
         expstr = ["capri login: ", "\# "]
         try:
-            session.sendline(cmd)
-            session.sendline("\n")
+            session.send(cmd)
+            session.send("\r")
             i = session.expect(expstr, 15)
             if i == 0:
                 session.sendline(self.usr)
@@ -40,7 +40,7 @@ class nic_con:
             session.expect("\$ ")
             return 0
         except pexpect.TIMEOUT:
-            print "=== Faled to stop UART session ==="
+            print "=== TIMEOUT: Faled to stop UART session ==="
             return -1
 
     def change_rate(self, orig_rate=115200, tgt_rate=9600):
@@ -49,18 +49,44 @@ class nic_con:
 
         cmd = self.fmt_change_rate.format(tgt_rate)
         session.sendline(cmd)
-        session.sendline("\n")
+        session.sendline("\r")
+
+        self.uart_session_stop(session)
+        common.session_stop(session)
+
+    def enable_mgmt(self, rate=9600):
+        session = common.session_start()
+        self.uart_session_start(session, rate)
+
+        session.timeout = 60
+        cmd = "sh /mnt/load_mnic.sh"
+        try:
+            session.sendline(cmd)
+            session.expect("\# ")
+        except:
+            print "=== TIMEOUT: Faled to enable management port ==="
+            return -1
 
         self.uart_session_stop(session)
         common.session_stop(session)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Diagnostic inteface", formatter_class=argparse.RawTextHelpFormatter)
-    #group = parser.add_mutually_exclusive_group()
-    parser.add_argument("-or", "--orig_rate", help="Original baud rate, e.g. 115200", type=int, default=115200)
-    parser.add_argument("-tr", "--tgt_rate", help="Target baud rate, e.g. 9600", type=int, default=9600)
+    parser = argparse.ArgumentParser(description="Diagnostic inteface", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    group = parser.add_mutually_exclusive_group()
+
+    group.add_argument("-br", "--change_baud_rate", help="Change baud rate", action='store_true')
+    group.add_argument("-mgmt", "--ena_mgmt_port", help="Enable managment port", action='store_true')
+
+    parser.add_argument("-or", "--orig_rate", help="Original baud rate", type=int, default=115200)
+    parser.add_argument("-tr", "--tgt_rate", help="Target baud rate", type=int, default=9600)
     args = parser.parse_args()
     
     con = nic_con()
-    session = con.change_rate(args.orig_rate, args.tgt_rate)
 
+    if args.change_baud_rate == True:
+        con.change_rate(args.orig_rate, args.tgt_rate)
+        sys.exit()
+
+    if args.ena_mgmt_port == True:
+        con.enable_mgmt(args.tgt_rate)
+        sys.exit()
---
+++ diag/python/regression/scripts/con_connect.sh
@@ -0,0 +1,5 @@
+# !/bin/bash
+
+cpldutil -cpld-wr -addr=0x18 -data=$1
+picocom -b 9600 -f h /dev/ttyS1
+
