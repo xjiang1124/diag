b5c8451e913f03f95e9be72f5e23d974d69bddfc
Xiaodong Hu <xiaodhu@pensando.io>
2018-11-12 13:26:06 -0800

    update sys_sanity.sh to fix slot10 bug

--- diag/scripts/sys_sanity.sh
+++ diag/scripts/sys_sanity.sh
@@ -1,9 +1,16 @@
 # !/bin/bash
 
 printf "====== jtag_cpurd_debug ======\n"
-jtag_cpurd_debug rst 0xa $1
-jtag_cpurd_debug ena 0xa $1
-jtag_cpurd_debug rd  0xa $1 0x6a000000 2
+if [ $1 -eq 10 ]
+then
+    jtag_cpurd_debug rst 0xa 0xa
+    jtag_cpurd_debug ena 0xa 0xa
+    jtag_cpurd_debug rd  0xa 0xa 0x6a000000 2
+else
+    jtag_cpurd_debug rst 0xa $1
+    jtag_cpurd_debug ena 0xa $1
+    jtag_cpurd_debug rd  0xa $1 0x6a000000 2
+fi
 
 printf "\n====== lsusb ======\n"
 lsusb
