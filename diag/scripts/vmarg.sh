84506703948503c60b149344f8462d575904279d
Xiaodong Hu <xiaodhu@pensando.io>
2019-02-14 18:34:22 -0800

    remove CAP0_3V3 from vmarg

--- diag/scripts/vmarg.sh
+++ diag/scripts/vmarg.sh
@@ -10,7 +10,7 @@ else
     percent=0
 fi
 
-for dev in CAP0_CORE_DVDD CAP0_ARM CAP0_3V3 CAP0_HBM CAP0_CORE_AVDD
+for dev in CAP0_CORE_DVDD CAP0_ARM CAP0_HBM CAP0_CORE_AVDD
 do
     devmgr -dev=$dev -margin -pct=$percent
 done
