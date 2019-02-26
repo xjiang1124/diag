62a80843b8cdb5dd7f30252165968e5abc6cc415
Xin Guo <xin.guo@pensando.io>
2019-02-25 23:41:38 -0800

    L1 test to support Naples25

--- diag/scripts/asic/l1_test.tcl
+++ diag/scripts/asic/l1_test.tcl
@@ -13,9 +13,17 @@ set ASIC_GEN "$ASIC_SRC"
 cd $ASIC_SRC/ip/cosim/tclsh
 source .tclrc.diag.new
 
+set uut "UUT_$slot_id"
+set card_type $::env($uut)
+if { $card_type == "NAPLES25" } {
+    set core_freq 417.0
+} else {
+    set core_freq 833.0
+}
+puts "card type: $card_type; UUT: $uut"
+
 puts "brd_num: $brd_num; slot_id: $slot_id"
 diag_open_j2c_if 10 $slot_id
-source $ASIC_SRC/ip/cosim/capri/cap_l1_tests.tcl
 set err_cnt [cap_l1_screen $brd_num 10 $slot_id 0 "" 0 1 0 1 1 833.0 $int_lpbk]
 
 # Print twice for DSP to capture signature
