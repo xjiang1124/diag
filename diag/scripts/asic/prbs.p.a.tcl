6903116ef5ee3a06c3c0b8509e6827a78d1a43d4
Xin Guo <xin.guo@pensando.io>
2019-06-17 16:31:57 -0700

    NIC PCIe prbs working

--- diag/scripts/asic/prbs.p.a.tcl
+++ diag/scripts/asic/prbs.p.a.tcl
@@ -5,7 +5,7 @@ puts "card_type: $card_type"
 set srds_list [cap_get_pcie_srds_list $card_type]
 puts "srds_list: $srds_list"
 cap_upload_pcie_spico
-cap_pcie_srds_prbs $srds_list 60 all 0
+cap_pcie_srds_prbs $srds_list 60 16g 0 prbs31 1
 
 puts "Snake Done"
 
