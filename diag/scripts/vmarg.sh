78c2d9faf43293f04b6cb63adfa902c981db31ae
Xiaodong Hu <xiaodhu@pensando.io>
2019-07-16 16:08:18 -0700

    adjust vmarg step by step

--- diag/scripts/vmarg.sh
+++ diag/scripts/vmarg.sh
@@ -1,17 +1,39 @@
 # !/bin/bash
 
-if [ "$1"  == "low" ]
+if [ "$1" == "normal" ]
 then
-    percent=-5
-elif [ "$1" == "high" ]
-then
-    percent=5
-else
     percent=0
+    for dev in CAP0_CORE_DVDD CAP0_ARM CAP0_HBM CAP0_CORE_AVDD
+    do
+        devmgr -dev=$dev -margin -pct=$percent
+    done
+else
+    if [ "$1"  == "low" ]
+    then
+        percent3=-3
+        percent4=-4
+        percent5=-5
+	for percent in $percent3 $percent4 $percent5
+	do
+	    for dev in CAP0_CORE_DVDD CAP0_ARM CAP0_HBM CAP0_CORE_AVDD
+            do
+                devmgr -dev=$dev -margin -pct=$percent
+            done
+	done
+    elif [ "$1" == "high" ]
+    then
+        percent3=3
+        percent4=4
+        percent5=5
+	for percent in $percent3 $percent4 $percent5
+        do
+            for dev in CAP0_CORE_DVDD CAP0_ARM CAP0_HBM CAP0_CORE_AVDD
+            do
+                devmgr -dev=$dev -margin -pct=$percent
+            done
+        done
+    else
+        echo "Invalid parameter"
+    fi
 fi
-
-for dev in CAP0_CORE_DVDD CAP0_ARM CAP0_HBM CAP0_CORE_AVDD
-do
-    devmgr -dev=$dev -margin -pct=$percent
-done
 
