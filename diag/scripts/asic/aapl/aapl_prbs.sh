082c288be375c306c9ba79b5418f94499fdce3a1
Xin Guo <xin.guo@pensando.io>
2019-08-05 11:03:07 -0700

    aapl working script with asicutil

--- diag/scripts/asic/aapl/aapl_prbs.sh
+++ diag/scripts/asic/aapl/aapl_prbs.sh
@@ -1,16 +1,18 @@
+#!/bin/bash
 echo $CARD_TYPE
 
+function do_prbs() {
 SERVER_IP=localhost
 PORT=9000
 duration=2
 
 sbus_list="2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32"
-#sbus_list="2"
+#sbus_list="2 18"
 for sbus in $sbus_list
 do
     echo "=== $SERVER_IP:$PORT $sbus ==="
     aapl dfe -server $SERVER_IP -port $PORT -addr $sbus -pcal-tune
-    #aapl dfe -server $SERVER_IP -port $PORT -addr $sbus -start-adaptive
+    aapl dfe -server $SERVER_IP -port $PORT -addr $sbus -start-adaptive
     aapl serdes -server $SERVER_IP -port $PORT -addr $sbus -display
 done
 
@@ -35,3 +37,6 @@ do
     sleep $duration
     aapl serdes -server $SERVER_IP -port $PORT -addr $sbus -ber -ber-dwell 1
 done
+}
+
+do_prbs | tee aapl.log
--- diag/scripts/asic/aapl/aapl_prbs_init.sh
+++ diag/scripts/asic/aapl/aapl_prbs_init.sh
@@ -6,10 +6,11 @@ PORT=9000
 sbus_list="2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32"
 #sbus_list="2 18"
 #sbus_list="4 6 8 10 12 14 16 20 22 24 26 28 30 32"
+
 for sbus in $sbus_list
 do
     echo "$SERVER_IP:$PORT $sbus"
-    aapl serdes-init -server $SERVER_IP -port $PORT -addr $sbus -reset 
+    aapl  serdes-init -server $SERVER_IP -port $PORT -addr $sbus -firm /data/pcie/serdes.0x1094_2347.rom
     #sleep 0.5
+    aapl  serdes-init -server $SERVER_IP -port $PORT -addr $sbus -div 160 -width 40 -elb -disable-signal-ok
 done
-
