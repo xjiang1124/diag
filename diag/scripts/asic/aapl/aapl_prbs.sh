7c718b081805ece2753821876816d6863552bd46
Xin Guo <xin.guo@pensando.io>
2019-08-06 11:05:44 -0700

    eth PRBS script update

--- diag/scripts/asic/aapl/aapl_prbs.sh
+++ diag/scripts/asic/aapl/aapl_prbs.sh
@@ -4,10 +4,9 @@ echo $CARD_TYPE
 function do_prbs() {
     SERVER_IP=localhost
     PORT=9000
-    duration=2
+    duration=60
     
     sbus_list="2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32"
-    #sbus_list="2 18"
     for sbus in $sbus_list
     do
         echo "=== $SERVER_IP:$PORT $sbus ==="
@@ -34,7 +33,6 @@ function do_prbs() {
     for sbus in $sbus_list
     do
         echo "=== $SERVER_IP:$PORT $sbus ==="
-        sleep $duration
         aapl serdes -server $SERVER_IP -port $PORT -addr $sbus -ber -ber-dwell 1
     done
 
--- diag/scripts/asic/aapl/eth_aapl_prbs.sh
+++ diag/scripts/asic/aapl/eth_aapl_prbs.sh
@@ -1,45 +1,82 @@
 #!/bin/bash
+
+if [ $# -ge 1 ]
+then
+    POLY=$1
+else
+    POLY="PRBS31"
+fi
+
+if [ $# -ge 2 ]
+then
+    DURATION=$2
+else
+    DURATION=60
+fi
+
+echo "POLY: $POLY; DURA: $DURATION"
+
 echo $CARD_TYPE
+card_type=$CARD_TYPE
 
 function do_prbs() {
     SERVER_IP=localhost
     PORT=9000
-    duration=2
+    $DURATION=60
     
-    sbus_list="34 35 36 37 38 39 40 41"
-    #sbus_list="2 18"
-    for sbus in $sbus_list
+    #sbus_list_naples100="34 35 36 37 38 39 40 41"
+    declare -a sbus_list_naples100=("34" "35" "36" "37" "38" "39" "40" "41")
+    declare -a atten_list_naples100=("0" "0" "0" "0" "0" "0" "0" "0")
+    declare -a pre_list_naples100=("2" "2" "2" "2" "2" "2" "2" "2")
+    declare -a post_list_naples100=("12" "12" "12" "12" "12" "12" "12" "12")
+
+    if [ $card_type = "NAPLES100" ]
+    then
+        sbus_list=("${sbus_list_naples100[@]}") 
+        atten_list=("${atten_list_naples100[@]}") 
+        pre_list=("${pre_list_naples100[@]}") 
+        post_list=("${post_list_naples100[@]}") 
+    fi
+
+    for idx in "${!sbus_list[@]}"
     do
-        echo "=== $SERVER_IP:$PORT $sbus ==="
-        aapl serdes -server $SERVER_IP -port $PORT -addr $sbus -atten 0 -pre 2 -post 12
+        sbus=${sbus_list[$idx]}
+        atten=${atten_list[$idx]}
+        pre=${pre_list[$idx]}
+        post=${post_list[$idx]}
+
+        echo "=== $SERVER_IP:$PORT sbus $sbus atten $atten pre $pre post $post ==="
+        aapl serdes -server $SERVER_IP -port $PORT -addr $sbus -atten 0 -pre $pre -post $post
         aapl dfe -server $SERVER_IP -port $PORT -addr $sbus -pcal-tune
         aapl dfe -server $SERVER_IP -port $PORT -addr $sbus -start-adaptive
         aapl serdes -server $SERVER_IP -port $PORT -addr $sbus -display
     done
     
-    for sbus in $sbus_list
+    for sbus in ${sbus_list[@]}
     do
         echo "=== $SERVER_IP:$PORT $sbus ==="
-        aapl serdes -server $SERVER_IP -port $PORT -addr $sbus -tx-data-sel PRBS31 -rx-mode PRBS31
+        aapl serdes -server $SERVER_IP -port $PORT -addr $sbus -tx-data-sel $POLY -rx-mode $POLY
     done
     
     
-    for sbus in $sbus_list
+    #for sbus in $sbus_list
+    for sbus in ${sbus_list[@]}
     do
         echo "=== $SERVER_IP:$PORT $sbus ==="
         aapl serdes -server $SERVER_IP -port $PORT -addr $sbus -error-reset
     done
     
-    sleep $duration
+    sleep $DURATION
     
-    for sbus in $sbus_list
+    #for sbus in $sbus_list
+    for sbus in ${sbus_list[@]}
     do
         echo "=== $SERVER_IP:$PORT $sbus ==="
-        sleep $duration
         aapl serdes -server $SERVER_IP -port $PORT -addr $sbus -ber -ber-dwell 1
     done
 
     echo "AAPL PRBS DONE"
 }
 
-do_prbs | tee /data/nic_arm/aapl/aapl.log
+#do_prbs | tee /data/nic_arm/aapl/aapl.log
+do_prbs
