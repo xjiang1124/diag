0fcebfdbeb9e247a8c538ba5ee81a3c628373bf7
Xiaodong Hu <xiaodhu@pensando.io>
2019-10-10 11:46:52 -0700

    try to output more msg

--- diag/mfg/mtp_sw_script/nic_profile.py
+++ diag/mfg/mtp_sw_script/nic_profile.py
@@ -7,7 +7,7 @@ import os
 
 def cleanup():
     try:
-        x = subprocess.check_output("penctl delete naples-profile -n new_naples_profile", env=naples_env, shell=True, stderr=subprocess.DEVNULL)
+        x = subprocess.check_output("penctl delete naples-profile -n new_naples_profile", env=naples_env, shell=True)
     except subprocess.CalledProcessError as e:
         print("Delete naples profile failed")
     pass
@@ -16,20 +16,20 @@ naples_env = os.environ.copy()
 naples_env["NAPLES_URL"] = "http://localhost"
 
 try:
-    x = subprocess.check_output("penctl create naples-profile -p disable -n new_naples_profile", env=naples_env, shell=True, stderr=subprocess.DEVNULL)
+    x = subprocess.check_output("penctl create naples-profile -p disable -n new_naples_profile", env=naples_env, shell=True)
 except subprocess.CalledProcessError as e:
     print("ERROR Create naples profile failed")
     raise
 
 try:
-    x = subprocess.check_output("penctl update naples -f new_naples_profile", env=naples_env, shell=True, stderr=subprocess.DEVNULL)
+    x = subprocess.check_output("penctl update naples -f new_naples_profile", env=naples_env, shell=True)
 except subprocess.CalledProcessError as e:
     print("ERROR Update naples profile failed")
     cleanup()
     raise
 
 try:
-    x = subprocess.check_output("penctl show naples-profiles", env=naples_env, shell=True, stderr=subprocess.DEVNULL)
+    x = subprocess.check_output("penctl show naples-profiles", env=naples_env, shell=True)
 except subprocess.CalledProcessError as e:
     print("ERROR Get naples profile failed")
     cleanup()
