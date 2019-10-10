506c5904d37c122856eb2ffb9c16c145ad83a1fb
Xiaodong Hu <xiaodhu@pensando.io>
2019-10-10 11:43:30 -0700

    add Netapp in output msg

--- diag/mfg/mtp_sw_script/nic_profile.py
+++ diag/mfg/mtp_sw_script/nic_profile.py
@@ -40,7 +40,7 @@ for item in profiles:
     if item["meta"]["name"] == "new_naples_profile":
         # print(item["spec"]["default-port-admin"])
         if item["spec"]["default-port-admin"] == "PORT_ADMIN_STATE_DISABLE":
-            print("PASS Profile disable port successfully")
+            print("PASS Netapp Profile disable port successfully")
         else:
-            print("ERROR Profile disable port failed")
+            print("ERROR Netapp Profile disable port failed")
             cleanup()
