78782f288a43e83b4b1cc264c2b5d52f07c48757
Xin Guo <xin.guo@pensando.io>
2020-01-14 15:21:41 -0800

    CPLD common file

---
+++ diag/app/src/device/cpld/nicCpldCommon/naplesCpldCommon.go
@@ -0,0 +1,48 @@
+package nicCpldCommon
+
+const (
+    ID_NAPLES100       = 0x12
+    ID_NAPLES25        = 0x13
+    ID_FORIO           = 0x14
+    ID_VOMERO          = 0x15
+    ID_TURIN           = 0x16
+    ID_NAPLES25SWM     = 0x17
+    ID_ELBA_ORACLE     = 0x18
+    ID_NAPLES25OCP     = 0x19
+    ID_MTP_OCP_ADAP    = 0x1A
+    ID_MTP_SWM_ADAP    = 0x1B
+)
+
+const (
+    REG_REV                = 0x00
+    REG_CTRL               = 0x01
+    REG_CTRL_STS           = 0x02
+    REG_INTR_STS           = 0x03
+    REG_LED_CTRL           = 0x04
+    REG_LED_RATE           = 0x05
+    REG_ESW_MDIO_CTRL_LOW  = 0x06
+    REG_ESW_MDIO_CTRL_HIGH = 0x07
+    REG_ESW_MDIO_DATA_LOW  = 0x08
+    REG_ESW_MDIO_DATA_HIGH = 0x09
+    REG_SLOT_ID            = 0x0A
+    REG_SMB_WR_SPI_RD_0    = 0x0B
+    REG_SMB_WR_SPI_RD_1    = 0x0C
+    REG_SPI_WR_SMB_RD_0    = 0x0D
+    REG_SPI_WR_SMB_RD_1    = 0x0E
+    REG_ASIC_CTRL_0        = 0x10
+    REG_ASIC_CTRL_1        = 0x11
+    REG_ASIC_CTRL_2        = 0x12
+    REG_ASIC_GPIO_0        = 0x13
+    REG_ASIC_GPIO_1        = 0x14
+    REG_ASIC_PSST          = 0x20
+    REG_ASIC_PIN_STAT_0    = 0x26
+    REG_ASIC_PIN_STAT_1    = 0x27
+    REG_ASIC_PIN_STAT_2    = 0x28
+    REG_PUFF_ERR_LMT       = 0x29
+    REG_PUFF_ERR_CNT       = 0x2A
+    REG_POWER_POST         = 0x30
+    REG_POWER_STAT0        = 0x31
+    REG_POWER_STAT1        = 0x32
+    REG_ID                 = 0x80
+)
+
