8b035e00beb74f0051f877728d8bf6c6a3dbe582
Xin Guo <xin.guo@pensando.io>
2020-01-17 13:05:27 -0800

    SWM disable adaptor sgmii port

---
+++ diag/scripts/swm_dis_sgmii.sh
@@ -0,0 +1,29 @@
+# !/bin/bash
+
+if [[ $# -ne 1 ]]
+then
+    echo "Invalid number of argument"
+    exit 0
+fi
+
+uut=$1
+turn_on_hub.sh $1
+
+uut_type=$(env|grep UUT_$uut)
+if [[ $uut_type != *"NAPLES25SWM"* ]]
+then
+    echo "Not a SWM, no need to turn off SGMII"
+fi
+
+phy=0x10
+# Read status
+adaptor_mdio_rd.sh $phy 0
+# Disable port -- seems not working
+adaptor_mdio_wr.sh $phy 4 0x7b
+# Force link down
+adaptor_mdio_wr.sh $phy 1 0x13
+# Read status
+adaptor_mdio_rd.sh $phy 0
+
+echo "SGMII on SWM adaptor card is disabled"
+
