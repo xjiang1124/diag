0bd2afa916c49868ee50fdbc9435e649243b0d0e
Xin Guo <xin.guo@pensando.io>
2020-04-08 16:19:59 -0700

    manfuacture log parser starter

--- diag/python/lib/common.py
+++ diag/python/lib/common.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import datetime
+import errno
+import os
 import pexpect
 import sys
 import time
@@ -26,7 +28,7 @@ def load_yaml(filename):
             #config_dict = yaml.load(stream)
             config = ordered_load(stream, yaml.SafeLoader)
         except yaml.YAMLError as exc:
-            print exc
+            print(exc)
     return config
 
 #=========================================================
@@ -98,7 +100,7 @@ def runcmd(cmd, timeout=30, sudo=False):
                 session.sendline(CONST.SUDO_PWD)
                 session.expect(pexpect.EOF, timeout=timeout)
     except pexpect.TIMEOUT:
-        print "=== TIMEOUT:", cmd, "==="
+        print("=== TIMEOUT:", cmd, "===")
         return -1
     return 0
 
@@ -120,7 +122,7 @@ def run_cmd(cmd, timeout=30, sudo=False):
         return 0
 
     except pexpect.TIMEOUT:
-        print "=== TIMEOUT:", cmd, "==="
+        print("=== TIMEOUT:", cmd, "===")
         session.close()
         return -2
 
@@ -153,7 +155,7 @@ def bash_cmd(cmd, timeout=30, sudo=False):
         return retval
 
     except pexpect.TIMEOUT:
-        print "=== TIMEOUT:", cmd, "==="
+        print("=== TIMEOUT:", cmd, "===")
         session.close()
         return -2
 
@@ -184,7 +186,7 @@ def session_cmd(session, cmd, timeout=30, sudo=False, ending=bash_prompt):
             return -1
 
     except pexpect.TIMEOUT:
-        print "=== TIMEOUT:", cmd, "==="
+        print("=== TIMEOUT:", cmd, "===")
         # Send CTRL+C
         session.send(chr(3))
         time.sleep(0.05)
@@ -214,7 +216,7 @@ def session_cmd_pass(session, cmd, pass_sign="", timeout=30, ending=bash_prompt,
         else:
             return -1
     except pexpect.TIMEOUT:
-        print "=== TIMEOUT:", cmd, "==="
+        print("=== TIMEOUT:", cmd, "===")
         # Send CTRL+C
         session.send(chr(3))
         time.sleep(0.05)
@@ -246,7 +248,7 @@ def session_cmd_pass_multi(session, cmd, pass_sign_list=["NO PASS SIGN"], timeou
         return 0
 
     except pexpect.TIMEOUT:
-        print "=== TIMEOUT:", cmd, "==="
+        print("=== TIMEOUT:", cmd, "===")
         # Send CTRL+C
         session.send(chr(3))
         time.sleep(0.05)
@@ -262,7 +264,7 @@ def session_start(timeout=30):
         session.expect(bash_prompt) 
         return session
     except pexpect.TIMEOUT:
-        print "=== Faled to spawn bash session ==="
+        print("=== Faled to spawn bash session ===")
         return None
 
 #=============================
@@ -274,6 +276,66 @@ def session_stop(session, timeout=30):
         session.expect(pexpect.EOF) 
         return 0
     except pexpect.TIMEOUT:
-        print "=== Faled to stop bash session ==="
+        print("=== Faled to stop bash session ===")
         return -1
 
+#=============================
+# mkdir -p
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    # Python 2.5
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            print("mkdir_p failed")
+            raise
+
+#=============================
+# untar
+def untar(fname):
+    if (fname.endswith("tar.gz")):
+        tar = tarfile.open(fname, "r:gz")
+        tar.extractall()
+        tar.close()
+    elif (fname.endswith("tar")):
+        tar = tarfile.open(fname, "r:")
+        tar.extractall()
+        tar.close()
+
+#=============================
+def find_file(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    if result == []:
+        print("Can not find file!", pattern, path)
+    return result
+
+#=============================
+def find_dir(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for dir_name in dirs:
+            if fnmatch.fnmatch(dir_name, pattern):
+                result.append(os.path.join(root, dir_name))
+    if result == []:
+        print("Can not find file!", pattern, path)
+
+    return result
+
+#=============================
+def find_first_dir(path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        #print path, root, dirs
+        for dir_name in dirs:
+            result.append(os.path.join(root, dir_name))
+
+    if result == []:
+        print("Can not find file!", path)
+
+    return result
---
+++ diag/python/mfg/mfg.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+import argparse
+import errno
+import os
+#import pathlib
+#import pexpect
+import re
+import subprocess
+import sys
+
+sys.path.append("../lib")
+import common
+
+class mfg:
+    def __init__(self):
+        self.cm_server = dict()
+        self.cm_server["FML"] = "192.168.1.112"
+        self.cm_server["FPN"] = "192.168.2.22"
+        self.user = "mfg"
+        self.pwd  = "pensando"
+        self.remote_path = "/mfg_log/"
+        self.local_path = "/home/xguo2/workspace/manufacture/mfg_log/"
+        self.log_path = "/home/xguo2/workspace/manufacture/test_log/"
+
+    def fetch_remote(self, cm, card_type, stage, sn_list):
+        remote_ip = mfg.cm_server[cm]
+
+        corner_4C = ['4C-H', '4C-L']
+        for sn in sn_list:
+            remote_path = self.remote_path+card_type+'/'+stage
+            local_path = self.local_path+card_type+'/'+stage
+            if stage == "4C":
+                for corner in corner_4C:
+                    remote_path1 = remote_path+'/'+corner+'/'+sn+'/'
+                    local_path1 = local_path+'/'+corner+'/'+sn+'/'
+                    common.mkdir_p(local_path1)
+
+                    cmd = "sshpass -p 'pensando' rsync -r mfg@"+remote_ip+":"+remote_path1+"* "+local_path1
+                    print(cmd)
+                    try:
+                        subprocess.run(cmd, shell=True, check=True, executable='/bin/bash')
+                    except:
+                        continue
+
+            else:
+                remote_path = remote_path+'/'+sn+'/'
+                local_path = local_path+'/'+sn+'/'
+                #pathlib.Path(local_path).mkdir(parents=True, exist_ok=True)
+                common.mkdir_p(local_path)
+
+                cmd = "sshpass -p 'pensando' scp -r mfg@"+remote_ip+":"+remote_path+"* "+local_path
+                print(cmd)
+                subprocess.run(cmd, shell=True, check=True, executable='/bin/bash')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Diagnostic inteface", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    group = parser.add_mutually_exclusive_group()
+
+    parser.add_argument("-fetch", "--fetch", help="Fetch log file", action='store_true')
+
+    group.add_argument("-cm", "--cm", help="CM site: FML/FPN", type=str, default="FPN")
+    parser.add_argument("-sn", "--sn", help="Serial Number", type=str, default="")
+    parser.add_argument("-sn_list", "--sn_list", help="Serial Number list", type=str, default="")
+    parser.add_argument("-card_type", "--card_type", help="Serial ", type=str, default="NAPLES100")
+    parser.add_argument("-stage", "--stage", help="P2C", type=str, default="NAPLES100")
+
+    args = parser.parse_args()
+    
+    mfg = mfg()
+
+    cm = args.cm.upper()
+    sn_list1 = args.sn_list.upper()
+    sn_list = sn_list1.split(',')
+    
+    stage = args.stage.upper()
+    card_type = args.card_type.upper()
+
+    try:
+        srv_ip = mfg.cm_server[cm]
+        print(srv_ip)
+    except KeyError:
+        print("Invalid CM:", cm)
+        sys.exit()
+
+
+    if args.fetch == True:
+        mfg.fetch_remote(cm, card_type, stage, sn_list)
+        sys.exit()
+
