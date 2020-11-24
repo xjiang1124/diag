5855a4968359c7135552088f25c6de32c0f4161e
David Wang <dwang@hw-srv2.pensando.io>
2020-11-24 15:00:24 -0800

    added vmarg for ortano support

--- diag/scripts/vmarg.sh
+++ diag/scripts/vmarg.sh
@@ -1,12 +1,26 @@
 # !/bin/bash
 
+set_vmarg()
+{
+    echo $CARD_TYPE
+    if [[ $CARD_TYPE == "ORTANO" ]]
+    then
+        for dev in ELB0_CORE ELB0_ARM VDD_DDR VDDQ_DDR
+        do
+            /data/nic_util/devmgr -dev=$dev -margin -pct=$1
+        done
+    else
+        for dev in CAP0_ARM CAP0_CORE_DVDD CAP0_HBM CAP0_CORE_AVDD
+        do
+            /data/nic_util/devmgr -dev=$dev -margin -pct=$1
+        done
+    fi
+}
+
 if [ "$1" == "normal" ]
 then
     percent=0
-    for dev in CAP0_ARM CAP0_CORE_DVDD CAP0_HBM CAP0_CORE_AVDD
-    do
-        devmgr -dev=$dev -margin -pct=$percent
-    done
+    set_vmarg $percent
 else
     if [ "$1"  == "low" ]
     then
@@ -17,10 +31,7 @@ else
         percent5=-5
 	for percent in $percent3 $percent4 $percent5
 	do
-	    for dev in CAP0_ARM CAP0_CORE_DVDD CAP0_HBM CAP0_CORE_AVDD
-            do
-                devmgr -dev=$dev -margin -pct=$percent
-            done
+            set_vmarg $percent
 	done
     elif [ "$1" == "high" ]
     then
@@ -32,10 +43,7 @@ else
 	for percent in $percent1 $percent2 $percent3 $percent4 $percent5
         do
             echo "=== Vmarg is at $percent% ==="
-            for dev in CAP0_ARM CAP0_CORE_DVDD CAP0_HBM CAP0_CORE_AVDD
-            do
-                devmgr -dev=$dev -margin -pct=$percent
-            done
+            set_vmarg $percent
         done
     else
         echo "Invalid parameter"
