d1efda7a73ea100a860dd0505240805fa3aa2267
David Wang <dwang@hw-srv2.pensando.io>
2021-02-19 15:57:36 -0800

    changed sys_sanity.sh from looking at the mtp chassis to nic cpld id

--- diag/scripts/sys_sanity.sh
+++ diag/scripts/sys_sanity.sh
@@ -1,9 +1,10 @@
 # !/bin/bash
 
-printf "====== jtag_cpurd_debug ======\n"
-mtp_id_cmd=$(cpldutil -cpld-rd -addr=0x80)
-mtp_id_str=($mtp_id_cmd)
-mtp_id=${mtp_id_str[-1]}
+res=$(smbutil -uut=uut_$1 -dev=CPLD -rd -addr=0x80)
+IFS=';'
+read -ra reg <<< "$res"
+IFS='='
+read -ra data <<< "${reg[1]}"
 if [ $1 -eq 10 ]
 then
     portnum=0xa
@@ -11,12 +12,14 @@ else
     portnum=$1
 fi
 
-if [ $mtp_id == "0x42" ]
+if [[ ${data[1]} == "0x43" ]] || [[ ${data[1]} == "0x44" ]] 
 then
+    printf "====== jtag_cpurd_v2 ======\n"
     jtag_cpurd_v2 rst 0xa $portnum
     jtag_cpurd_v2 ena 0xa $portnum
     jtag_cpurd_v2 rd  0xa $portnum 0x307c0000 2
 else
+    printf "====== jtag_cpurd_debug ======\n"
     jtag_cpurd_debug rst 0xa $portnum 
     jtag_cpurd_debug ena 0xa $portnum
     jtag_cpurd_debug rd  0xa $portnum 0x6a000000 2
