bc4993db919889255e9da66ce245fd40e3c6905f
David Wang <dwang@hw-srv2.pensando.io>
2021-03-05 00:30:54 -0800

    add elb_pcie_init.tcl to setup pcie pll from mtp side.

---
+++ diag/scripts/asic/elb_pcie_init.tcl
@@ -0,0 +1,30 @@
+# !/usr/bin/tclsh
+#
+# package require cmdline
+
+source ./cmdline.tcl
+set parameters {
+    {slot.arg       ""          "Slot number"}
+    {port.arg       "0xa"       "jtag port number"}
+}
+
+array set arg [cmdline::getoptions argv $parameters]
+set slot        $arg(slot)
+set port        $arg(t)
+
+puts "slot: $slot port $port"
+
+
+cd /home/diag/diag/asic/asic_src/ip/cosim/tclsh
+source .tclrc.diag.elb
+
+diag_open_j2c_if $port $slot
+elb_soc_pcie_pll_init 0 0
+elb_soc_i_rom_enable 0 0
+elb_soc_pcie_pll_lock 0 0 -1
+csr_write {elb0.pp.pp[0].cfg_pp_sd_async_reset_n} 0xffff
+csr_write {elb0.pp.pp[1].cfg_pp_sd_async_reset_n} 0xffff
+diag_close_j2c_if $port $slot
+
+puts "FINISH SETTING UP PCIE PLL"
+
