78a9a45f71ae2bfea0e835610dccd47f87683bf5
David Wang <dwang@hw-srv2.pensando.io>
2021-03-05 22:03:39 -0800

    added slot list to initial multiple slot in one tcl session

--- diag/scripts/asic/elb_pcie_init.tcl
+++ diag/scripts/asic/elb_pcie_init.tcl
@@ -10,21 +10,23 @@ set parameters {
 
 array set arg [cmdline::getoptions argv $parameters]
 set slot        $arg(slot)
-set port        $arg(t)
+set port        $arg(port)
 
-puts "slot: $slot port $port"
-
+set slot_list [split $slot ""]
+puts "slot list $slot_list"
 
 cd /home/diag/diag/asic/asic_src/ip/cosim/tclsh
 source .tclrc.diag.elb
 
-diag_open_j2c_if $port $slot
-elb_soc_pcie_pll_init 0 0
-elb_soc_i_rom_enable 0 0
-elb_soc_pcie_pll_lock 0 0 -1
-csr_write {elb0.pp.pp[0].cfg_pp_sd_async_reset_n} 0xffff
-csr_write {elb0.pp.pp[1].cfg_pp_sd_async_reset_n} 0xffff
-diag_close_j2c_if $port $slot
-
+foreach slot $$slot_list {
+    puts "slot: $slot port $port"
+    diag_open_j2c_if $port $slot
+    elb_soc_pcie_pll_init 0 0
+    elb_soc_i_rom_enable 0 0
+    elb_soc_pcie_pll_lock 0 0 -1
+    csr_write {elb0.pp.pp[0].cfg_pp_sd_async_reset_n} 0xffff
+    csr_write {elb0.pp.pp[1].cfg_pp_sd_async_reset_n} 0xffff
+    diag_close_j2c_if $port $slot
+}
 puts "FINISH SETTING UP PCIE PLL"
 
