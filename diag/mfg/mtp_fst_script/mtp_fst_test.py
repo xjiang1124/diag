636f49e14ea5302f4b559841605211691c4fef08
nabeel-0 <nabeel@pensando.io>
2021-04-16 15:38:09 -0700

    FST broke for non-Ortano - fixed.

--- diag/mfg/mtp_fst_script/mtp_fst_test.py
+++ diag/mfg/mtp_fst_script/mtp_fst_test.py
@@ -591,16 +591,22 @@ def main():
 
     if "ORTANO" not in card_type:
         for _slot, _sn, _nic_type in fail_match:
-            if _slot in pass_nic_list:
-                pass_nic_list.remove(_slot)
-            if _slot not in fail_nic_list:
-                fail_nic_list.append(_slot)
+            slot = int(_slot)-1
+            mtp_mgmt_ctrl.mtp_set_nic_type(slot, _nic_type)
+            mtp_mgmt_ctrl.mtp_set_nic_sn(slot, _sn)
+            if slot in pass_nic_list:
+                pass_nic_list.remove(slot)
+            if slot not in fail_nic_list:
+                fail_nic_list.append(slot)
 
         for _slot, _sn, _nic_type in pass_match:
-            if _slot in fail_nic_list:
+            slot = int(_slot)-1
+            mtp_mgmt_ctrl.mtp_set_nic_type(slot, _nic_type)
+            mtp_mgmt_ctrl.mtp_set_nic_sn(slot, _sn)
+            if slot in fail_nic_list:
                 continue
-            if _slot not in pass_nic_list:
-                pass_nic_list.append(_slot)
+            if slot not in pass_nic_list:
+                pass_nic_list.append(slot)
 
     for slot in pass_nic_list:
         key = libmfg_utils.nic_key(slot)
