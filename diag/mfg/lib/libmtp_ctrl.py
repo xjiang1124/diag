881d33e8289d05363b96a5b30c27ae0a4c2367b6
nabeel-0 <nabeel@pensando.io>
2021-04-27 10:05:04 -0700

    SWM DELL ECO to release 1.14.4

--- diag/mfg/lib/libmtp_ctrl.py
+++ diag/mfg/lib/libmtp_ctrl.py
@@ -2161,7 +2161,7 @@ class mtp_ctrl():
                 self.cli_log_slot_err_lock(slot, "Check SWI Software Image: Software Image match to nic part number failed")
                 return False
         elif naples_pn[0:7] == "68-0014":     #NAPLES25 SWM DELL
-            if software_pn != "90-0002-0007":
+            if software_pn != "90-0007-0001":
                 self.cli_log_slot_err_lock(slot, "Check SWI Software Image: Software Image match to nic part number failed")
                 return False
         elif naples_pn[0:7] == "68-0019":     #NAPLES25 SWM 833
