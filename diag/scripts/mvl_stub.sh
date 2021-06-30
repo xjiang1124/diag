961de3d69c25665848e436afd30c65a50bd27a28
David Wang <dwang@hw-srv2.pensando.io>
2021-06-30 13:56:53 -0700

    fix stub test

--- diag/scripts/mvl_stub.sh
+++ diag/scripts/mvl_stub.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-./xo3dcpld -mdiowr 0x16 0x3 0x06
+./xo3dcpld -smiwr 0x16 0x3 0x06
 sleep 1
-./xo3dcpld -mdiowr 0x12 0x3 0x18
+./xo3dcpld -smiwr 0x12 0x3 0x18
 sleep 1
-./xo3dcpld -mdiowr 0x10 0x3 0x18
+./xo3dcpld -smiwr 0x10 0x3 0x18
 sleep 1
 
-p0=$(./xo3dcpld -mdiord 0x11 0x3)
-echo $p0 
-if [[ $(( $p0 & 0xff )) != 0 ]]; then
-    echo "MVL STUB TEST FAILED"
+p0=$(./xo3dcpld -smird 0x11 0x3)
+if [[ $p0 == 0 ]]; then
+    echo $p0 
+    echo "MVL STUB TEST FAILED -- no packet received"
     exit 1
 fi
 
-./xo3dcpld -mdiowr 0x16 0x3 0x0
-./xo3dcpld -mdiowr 0x12 0x3 0x0
-./xo3dcpld -mdiowr 0x10 0x3 0x0
+if [[ $(( $p0 & 0xff )) != 0 ]]; then
+    echo $p0 
+    echo "MVL STUB TEST FAILED -- packets with errors"
+    exit 1
+fi
+
+./xo3dcpld -smiwr 0x10 0x3 0x0
+./xo3dcpld -smiwr 0x12 0x3 0x0
+./xo3dcpld -smiwr 0x16 0x3 0x0
 
 echo "MVL STUB TEST PASSED"
    
