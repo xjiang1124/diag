bb4e49d5a8f291843bdcf8d6b7e4c5cfb1815298
nabeel-0 <nabeel@pensando.io>
2021-07-06 21:26:49 -0700

    New diagfw for ortano

--- diag/mfg/lib/libmfg_cfg.py
+++ diag/mfg/lib/libmfg_cfg.py
@@ -173,8 +173,8 @@ class NIC_IMAGES:
     fail_cpld_ver["ORTANO2"] = "0x83"
     fail_cpld_dat["ORTANO2"] = "0x07"
     fea_cpld_img["ORTANO2"] = "naples200_ortano2_fea_04272021.bin"
-    diagfw_img["ORTANO2"] = "elba_diagfw_1.15.8-C-3_2021.05.15.tar"
-    diagfw_dat["ORTANO2"] = "05-15-2021"
+    diagfw_img["ORTANO2"] = "elba_diagfw_lacona_0.2-1-g9feba69_2021.06.22.tar"
+    diagfw_dat["ORTANO2"] = "06-22-2021"
     goldfw_img["ORTANO2"] = "elba_goldfw_1.15.8-C-9_2021.05.22.tar"
     goldfw_dat["ORTANO2"] = "05-22-2021"
 
