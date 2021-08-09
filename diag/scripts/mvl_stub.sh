60f74d5b5866a096e9b72cf145bbf34355c8d31f
David Wang <dwang@hw-srv2.pensando.io>
2021-08-09 12:40:19 -0700

    added code for mvl_stub to read counter twice and compare

--- diag/scripts/mvl_stub.sh
+++ diag/scripts/mvl_stub.sh
@@ -6,16 +6,18 @@ sleep 1
 sleep 1
 ./xo3dcpld -smiwr 0x10 0x3 0x18
 sleep 1
-
 p0=$(./xo3dcpld -smird 0x11 0x3)
-if [[ $p0 == 0 ]]; then
+sleep 1
+p1=$(./xo3dcpld -smird 0x11 0x3)
+echo $p0 $p1
+
+if [[ $(( p0 & 0xff00 )) == 0 ]]; then
     echo $p0 
     echo "MVL STUB TEST FAILED -- no packet received"
     exit 1
 fi
 
-if [[ $(( $p0 & 0xff )) != 0 ]]; then
-    echo $p0 
+if [[ $(( $p0 & 0xff )) -lt $(( $p1 & 0xff )) ]]; then
     echo "MVL STUB TEST FAILED -- packets with errors"
     exit 1
 fi
