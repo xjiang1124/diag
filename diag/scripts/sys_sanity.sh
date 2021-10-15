24de318da2f1fafafa2e3a3c8e4fdedfb65695d5
David Wang <dwang@hw-srv2.pensando.io>
2021-10-15 15:57:58 -0700

    fixed a cut / past issue

--- diag/scripts/sys_sanity.sh
+++ diag/scripts/sys_sanity.sh
@@ -20,11 +20,11 @@ then
     jtag_cpurd_v2 ena 0xa $portnum
     jtag_cpurd_v2 rd  0xa $portnum 0x307c0000 2
 elif [[ $MTP_TYPE == "MTP_TURBO_ELBA" ]] 
+then
     printf "====== jtag_cpurd_v2 turbo ======\n"
     jtag_cpurd_v2 rst ${ftdi_addr[$portnum]} 1
     jtag_cpurd_v2 ena ${ftdi_addr[$portnum]} 1
     jtag_cpurd_v2 rd  ${ftdi_addr[$portnum]} 1 0x307c0000 2
-then
 else
     printf "====== jtag_cpurd_debug ======\n"
     jtag_cpurd_debug rst 0xa $portnum 
