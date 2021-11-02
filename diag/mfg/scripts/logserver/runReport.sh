1fa0a0d1c76f6e8ef4a701df1ea7e725c28d55e3
whwinson <winson@pensando.io>
2021-11-02 16:20:08 -0700

    Change Taormina from P2 to PP

--- diag/mfg/scripts/logserver/runReport.sh
+++ diag/mfg/scripts/logserver/runReport.sh
@@ -27,7 +27,7 @@ NAPLES100_input.json
 NAPLES100HPE_input.json
 NAPLES100IBM_input.json
 NAPLES25OCP_input.json
-TAORMINA_P2_2_input.json
+TAORMINA_PP_input.json
 FLEX_LACONA32DELL_P1B_input.json
 LACONA32DELL_input.json
 )
