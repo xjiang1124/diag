0f5a32ec10434735015c745833ba00a1dd3bf33d
Xin Guo <xin.guo@pensando.io>
2021-11-07 21:47:01 -0800

    Vmarg supports pomonte and lacona32

--- diag/scripts/vmarg.sh
+++ diag/scripts/vmarg.sh
@@ -1,67 +1,94 @@
-# !/bin/bash
+#!/bin/bash
+
+set_vmarg_lacona()
+{
+    if [[ $1 == "arm" ]]
+    then
+        vrail="ELB0_ARM"
+    else
+        vrail="ELB0_CORE"
+    fi
+
+    vout=$(/data/nic_util/devmgr -status | grep $vrail | sed "s/[ +]\]//g" | awk -F " " '{print $6}')
+    voutmv=$(awk "BEGIN {print $vout*1000*(100+$2)/100}")
+    voutmv=$(printf '%.0f' $voutmv)
+
+    echo "Calculated vout: $voutmv"
+    if [[ "$voutmv" -lt 700 ]]
+    then
+        voutmv=700
+        echo "Setting to minimal vout: $voutmv"
+    fi
+    /data/nic_util/devmgr -dev=$vrail -margin -mgmode=mv -vout=$voutmv
+}
 
 set_vmarg()
 {
     echo $CARD_TYPE
     if [[ $CARD_TYPE == "ORTANO" || $CARD_TYPE == "ORTANO2" ]]
     then
-        #for dev in ELB0_CORE ELB0_ARM VDD_DDR VDDQ_DDR
-        #for dev in VDD_DDR
-        #do
-        #    /data/nic_util/devmgr -dev=$dev -margin -pct=$1
-        #done
-        if [[ "$1" -lt 3 && "$1" -gt -1 ]] 
+        if [[ "$1" -lt 3 && "$1" -ge -2 ]] 
         then
-                /data/nic_util/devmgr -dev=ELB0_ARM -margin -pct=$1
-                /data/nic_util/devmgr -dev=ELB0_CORE -margin -pct=$1
-                /data/nic_util/devmgr -dev=VDDQ_DDR -margin -pct=$1
-                /data/nic_util/devmgr -dev=VDD_DDR -margin -pct=$1
+            /data/nic_util/devmgr -dev=ELB0_ARM -margin -pct=$1
+            /data/nic_util/devmgr -dev=ELB0_CORE -margin -pct=$1
+            /data/nic_util/devmgr -dev=VDDQ_DDR -margin -pct=$1
+            /data/nic_util/devmgr -dev=VDD_DDR -margin -pct=$1
+            return
         fi
-        if [[ "$1" == -3 ]]
+        echo "Skipping $1%"
+    elif [[ $CARD_TYPE == "LACONA32"        || \
+            $CARD_TYPE == "LACONA32DELL"    || \
+            $CARD_TYPE == "POMONTE"         || \
+            $CARD_TYPE == "POMONTEDELL"     ]]
+    then
+        if [[ "$1" -eq 0 ]]
         then
-            /data/nic_util/devmgr -dev=ELB0_ARM -margin -pct=-2
-            /data/nic_util/devmgr -dev=ELB0_CORE -margin -pct=-2
-            /data/nic_util/devmgr -dev=VDDQ_DDR -margin -pct=-2
-            /data/nic_util/devmgr -dev=VDD_DDR -margin -pct=-2
+            echo "Do nothing"
+            return
+        elif [[ "$1" -lt 3 && "$1" -ge -2 ]] 
+        then
+            /data/nic_util/devmgr -dev=ELB0_ARM -margin -pct=$1
+            /data/nic_util/devmgr -dev=VDDQ_DDR -margin -pct=$1
+            set_vmarg_lacona core $1
+            return
         fi
+        echo "Skipping $1%"
     else
         for dev in CAP0_ARM CAP0_CORE_DVDD CAP0_HBM CAP0_CORE_AVDD
         do
             /data/nic_util/devmgr -dev=$dev -margin -pct=$1
         done
     fi
+    return
 }
 
 if [ "$1" == "normal" ]
 then
     percent=0
     set_vmarg $percent
-else
-    if [ "$1"  == "low" ]
+elif [ "$1"  == "low" ]
+then
+    declare -a pct_list=(-1 -2 -3 -4 -5)
+    if [[ $ASIC_TYPE == "ELBA" ]]
     then
-        percent1=-1
-        percent2=-2
-        percent3=-3
-        percent4=-4
-        percent5=-5
-    for percent in $percent3 $percent4 $percent5
-    do
-            set_vmarg $percent
-    done
-    elif [ "$1" == "high" ]
-    then
-        percent1=1
-        percent2=2
-        percent3=3
-        percent4=4
-        percent5=5
-    for percent in $percent1 $percent2 $percent3 $percent4 $percent5
-        do
-            echo "=== Vmarg is at $percent% ==="
-            set_vmarg $percent
-        done
-    else
-        echo "Invalid parameter"
+        declare -a pct_list=(-1 -2)
     fi
+elif [ "$1" == "high" ]
+then
+    declare -a pct_list=(1 2 3 4 5)
+    if [[ $ASIC_TYPE == "ELBA" ]]
+    then
+        declare -a pct_list=(1 2)
+    fi
+else
+    echo "Invalid parameter"
+    exit 0
 fi
+
+for percent in ${pct_list[@]}
+do
+    echo "=== Setting Vmarg to $percent% ==="
+    set_vmarg $percent
+    echo "=== Vmarg is at $percent% ==="
+done
 
