0eea278703871e6520b0695c624ebf3f93ba7d99
nabeel zaim <70537820+nabeel-0@users.noreply.github.com>
2021-11-25 07:19:21 -0800

    Release capri image only if capri selected in jenkins

--- mfg-release-script.sh
+++ mfg-release-script.sh
@@ -1,5 +1,6 @@
 top_dir=$PWD
 release_name=$(echo $branch_or_tag | awk -F'/' '{print $NF}')
+asic_type=$(echo $asic_type)
 jenkins_dir=/vol/hw/diag/mfg_release/jenkins
 mfg_script_dir=${jenkins_dir}/${release_name}/mfg
 
@@ -17,13 +18,11 @@ chmod 777 $mfg_script_dir/release/
 sync
 
 ## COPY DIAG IMAGES
-cp build/images/image_amd64_elba.tar $mfg_script_dir/release/image_amd64_elba_${release_name}.tar
-cp build/images/image_arm64_elba.tar $mfg_script_dir/release/image_arm64_elba_${release_name}.tar
-cp build/images/image_amd64_capri.tar $mfg_script_dir/release/image_amd64_capri_${release_name}.tar
-cp build/images/image_arm64_capri.tar $mfg_script_dir/release/image_arm64_capri_${release_name}.tar
+cp build/images/image_amd64_${asic_type}.tar $mfg_script_dir/release/image_amd64_${asic_type}_${release_name}.tar
+cp build/images/image_arm64_${asic_type}.tar $mfg_script_dir/release/image_arm64_${asic_type}_${release_name}.tar
 
-sed -i "s/MTP_ARM64_IMAGE = \".*\.tar\"/MTP_ARM64_IMAGE = \"image_arm64_elba_${release_name}\.tar\"/g" $mfg_script_dir/lib/libmfg_cfg.py
-sed -i "s/MTP_AMD64_IMAGE = \".*\.tar\"/MTP_AMD64_IMAGE = \"image_amd64_elba_${release_name}\.tar\"/g" $mfg_script_dir/lib/libmfg_cfg.py
+sed -i "s/MTP_ARM64_IMAGE = \".*\.tar\"/MTP_ARM64_IMAGE = \"image_arm64_${asic_type}_${release_name}\.tar\"/g" $mfg_script_dir/lib/libmfg_cfg.py
+sed -i "s/MTP_AMD64_IMAGE = \".*\.tar\"/MTP_AMD64_IMAGE = \"image_amd64_${asic_type}_${release_name}\.tar\"/g" $mfg_script_dir/lib/libmfg_cfg.py
 
 ## PRUNING
 rm -rf $mfg_script_dir/scripts
