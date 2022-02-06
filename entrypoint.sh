400c3ca33fcb1d28fc347f36d435fb355d29461a
Leslie Qi Wang <qiwa@pensando.io>
2022-02-05 17:59:05 -0800

    enable amd64 build
    Signed-off-by: Leslie Qi Wang <qiwa@pensando.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@
 curl -o /usr/bin/asset-pull http://pm.test.pensando.io/tools/asset-pull && chmod +x /usr/bin/asset-pull
 asset-pull --help
 
+# prepare env
+# TODO: is it the final location?
+mkdir -p /psdiag/lib/third-party
+cp /vol/hw/diag/regression/qa_regression/psdiag_xin/diag/lib/third-party/libftd2xx.a /psdiag/lib/third-party/
+
 # debugging purpose to show where we are
 pwd & ls
 
