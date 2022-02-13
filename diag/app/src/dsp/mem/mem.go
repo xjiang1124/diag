f9fc7537fe5fe85a586d325f481a00bc907da182
Yanmin Hua <91923539+yanminhua2017@users.noreply.github.com>
2022-02-13 00:14:05 -0800

    Use available memory size in ddr stress test (#15)
    Signed-off-by: Yanmin Hua <yanmin@pensando.io>

--- diag/app/src/dsp/mem/mem.go
+++ diag/app/src/dsp/mem/mem.go
@@ -2,7 +2,9 @@ package main
 
 import (
     "os"
+    "os/exec"
     "flag"
+    "strings"
     "strconv"
 
     "common/diagEngine"
@@ -22,7 +24,6 @@ const (
 func MemDdr_StressHdl(argList []string) {
     fs := flag.NewFlagSet("FlagSet", flag.ContinueOnError)
     secondsPtr := fs.Int("seconds", 60, "number of seconds to run")
-    mbytesPtr := fs.Int("mbytes", 20000, "megabytes of ram to test")
     copy_threadsPtr := fs.Int("copy_threads", 16, "number of memory copy threads to run")
     logfilePtr := fs.String("logfile", "stressapptest.log", "log output to file 'logfile'")
     var cmd string
@@ -33,10 +34,25 @@ func MemDdr_StressHdl(argList []string) {
         dcli.Println("e", "Parse failed", errFs)
     }
 
+    out, errGo := exec.Command("grep", "MemAvailable", "/proc/meminfo").Output()
+    if errGo != nil {
+        dcli.Println("e", "Failed to get MemAvailable", errGo)
+        err = errType.FAIL
+        diagEngine.FuncMsgChan <- err
+        return
+    }
+    availMemStr := string(out)
+    availMemStrArr := strings.Fields(availMemStr)
+    availMemInKb := availMemStrArr[1]
+    availMemSize, _ := strconv.Atoi(availMemInKb)
+    dcli.Println("i", "Available memory in Kbytes: ", availMemSize)
+    availMemSize = (availMemSize / 1024 / 100) * 100;
+    dcli.Println("i", "Available memory in Mbytes: ", availMemSize)
+
     cmd = "/data/nic_util/stressapptest_arm"
     passSign := "Status: PASS - please verify no corrected errors"
     failSign := "Status: FAIL - test discovered HW problems"
-    err = runCmd.Run(passSign, failSign, cmd, "-M", strconv.Itoa(*mbytesPtr), "-s", strconv.Itoa(*secondsPtr), "-m", strconv.Itoa(*copy_threadsPtr), "-l", "/data/nic_util/" + *logfilePtr)
+    err = runCmd.Run(passSign, failSign, cmd, "-M", strconv.Itoa(availMemSize), "-s", strconv.Itoa(*secondsPtr), "-m", strconv.Itoa(*copy_threadsPtr), "-l", "/data/nic_util/" + *logfilePtr)
 
     // Inform diag engine that test handler is done
     // Use chan to return error code
