4d0e7042f5dfce97dc20d6d582d4e5a8196144c2
David Wang <61480369+davidxfwang8@users.noreply.github.com>
2022-03-24 14:21:40 -0700

    added arm prbs test through console option (#92)
    fixed some code orders in arm_l1.py to make more sense
    
    Co-authored-by: David Wang <dwang@hw-srv2.pensando.io>

--- diag/python/regression/arm_prbs.py
+++ diag/python/regression/arm_prbs.py
@@ -18,45 +18,44 @@ import common
 from nic_con import nic_con
 from nic_test import nic_test
 
-class arm_l1:
+class arm_prbs:
     def __init__(self):
-        self.name = "NIC arm L1"
+        self.name = "NIC PCIE"
         self.baud_rate = 115200
         self.nic_con = nic_con()
         self.nic_test = nic_test()
 
-    def test_start(self, slot=0, vmarg='normal', sn='SN00000000', mode='hod'):
+    def test_start(self, slot=0, vmarg='normal', lpbk=0, dura=60, poly="PRBS31", mode="PCIE"):
         if slot == 0 or slot > 10:
             print "Invalid slot number:", slot
             sys.exit(0)
         ret = 0
-        print "=== Starting NIC arm L1 on slot {} ===".format(slot)
+        print "=== Starting NIC arm {} prbs {} {} on slot {} ===".format(mode, dura, lpbk, slot)
 
         session = common.session_start()
         try:
             session.timeout = 30
             self.nic_con.uart_session_start_slot(session, self.baud_rate, slot)
             self.nic_con.uart_session_cmd(session, "/data/nic_arm/vmarg.sh {}".format(vmarg))
-            self.nic_con.uart_session_cmd(session, "cd /data/nic_arm/nic/asic_src/ip/cosim/tclsh")
-            self.nic_con.uart_session_cmd(session, "./diag.exe ../elba/elb_arm_l1.tcl {} {} 1 &".format(sn, mode))
+            self.nic_con.uart_session_cmd(session, "/data/nic_arm/nic/asic_src/ip/cosim/tclsh/nic_prbs.sh {} {} {} &".format(mode, dura, lpbk))
             self.nic_con.uart_session_stop(session)
 
-            print "=== NIC arm L1 on slot {} started ===".format(slot)
+            print "=== NIC arm {} prbs on slot {} started ===".format(mode, slot)
         except:
             self.nic_con.uart_session_stop(session)
-            print "=== NIC arm L1 on slot {} FAILED to start! ===".format(slot)
+            print "=== NIC arm {} prbs on slot {} FAILED to start! ===".format(mode, slot)
             common.session_stop(session)
             ret = -1
 
         common.session_stop(session)
         return ret
 
-    def test_check(self, slot=0, timeout=30):
+    def test_check(self, slot=0, mode="PCIE", timeout=30):
         if slot == 0 or slot > 10:
             print "Invalid slot number:", slot
             sys.exit(0)
         ret = 0
-        print "=== ARM L1 Checking result on slot {} ===".format(slot)
+        print "=== ARM {} prbs Checking result on slot {} ===".format(mode, slot)
 
         self.nic_con.switch_console(slot)
         session = common.session_start()
@@ -66,24 +65,32 @@ class arm_l1:
             common.session_stop(session)
             return -1
  
-        cmd = "tail -5 /data/nic_arm/nic/asic_src/ip/cosim/tclsh/elba_arm_l1_test.log"
-        ret = self.nic_con.uart_session_cmd_sig(session, cmd, 5, "\#", ["ARM L1 TESTS PASSED", "FAILED"], False)
-        if ret == 0:
-            print "ARM L1 TESTS PASSED"
-        elif ret == 1:
-            print "ARM L1 TESTS FAILED"
+        if mode == "PCIE":
+            cmd = "tail -5 /data/nic_arm/nic/asic_src/ip/cosim/tclsh/elba_PRBS_PCIE.log"
+            ret = self.nic_con.uart_session_cmd_sig(session, cmd, 5, "\#", ["PCIE PRBS PASSED", "FAILED"], False)
+        elif mode == "ETH":
+            cmd = "tail -5 /data/nic_arm/nic/asic_src/ip/cosim/tclsh/elba_PRBS_MX.log"
+            ret = self.nic_con.uart_session_cmd_sig(session, cmd, 5, "\#", ["MX PRBS PASSED", "FAILED"], False)
         else:
-            print "ARM L1 NO RESULT in log file or no log file found"
+            print "Invalid test type for checking result!!!"
+            return -1
+
+        if ret == 0:
+            print "ARM {} PRBS TEST PASSED".format(mode)
+        elif ret == 1:
+            print "ARM {} PRBS TEST FAILED".format(mode)
+        else:
+            print "ARM {} PRBS NO RESULT in log file or no log file found".format(mode)
 
         self.nic_con.uart_session_stop(session)
         common.session_stop(session)
 
         print "check_result:"
-        print "=== ARM L1 Checking result on slot {} Done ===".format(slot)
+        print "=== ARM {} PRBS Checking result on slot {} Done ===".format(mode, slot)
         return ret
 
-    def arm_l1(self, slot_list=[], wait_time=300, vmargin='normal', sn="SN00000000", mode="hod", no_pwr_cycle=False):
-        print "=== ARM L1"
+    def arm_prbs(self, slot_list=[], wait_time=300, vmargin='normal', lpbk=0, dura=60, poly="PRBS31", mode="PCIE", no_pwr_cycle=False):
+        print "=== ARM {} PRBS {} {} {}".format(mode, dura, lpbk, vmargin)
         if len(slot_list) == 0:
             print "No nic specified -- Exit"
             sys.exit(0)
@@ -91,7 +98,6 @@ class arm_l1:
         print "slot:", slot_list
         nic_list_remain = slot_list[:]
         nic_failed_setup = slot_list[:]
-        #ret, nic_list_remain = self.setup_env_multi(slot_list, no_pwr_cycle)
         ret, nic_failed_setup = self.nic_test.setup_env_multi_top(slot_list, False, 30, False, no_pwr_cycle, False, False, "elba", False, False)
 
         # initialize all slots to FAIL
@@ -105,7 +111,7 @@ class arm_l1:
 
         # Start L1 
         for slot in nic_list_remain:
-            ret = self.test_start(int(slot), vmargin)
+            ret = self.test_start(int(slot), vmargin, lpbk, dura, poly, mode)
             if ret == 0:
                 test_result[slot] = "NO RESULT"
 
@@ -117,7 +123,7 @@ class arm_l1:
             if test_result[slot] != "NO RESULT":
                 continue
 
-            test_sts = self.test_check(int(slot))
+            test_sts = self.test_check(int(slot), mode)
             if test_sts == 0:
                 print "=== Result at Slot {}: Passed".format(slot)
                 test_result[slot] = "PASSED"
@@ -126,42 +132,44 @@ class arm_l1:
                 test_result[slot] = "FAILED"
 
         # Print result
-        print "\n====== NIC ARM L1 TEST RESULT: ======"
+        print "\n====== NIC ARM {} PRBS TEST RESULT: ======".format(mode)
         result_fmt = "Slot {:<2}: {:<5}"
         for slot, sts in test_result.items():
             print result_fmt.format(slot, sts)
         print "======================================"
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="ARM L1 Interface", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(description="ARM PRBS Interface", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-test_start", "--l1_start", help="Start arm l1 test", action='store_true')
-    group.add_argument("-test_check", "--l1_check", help="Check arm l1 result", action='store_true')
-    group.add_argument("-arm_l1", "--arm_l1", help="Run ARM L1", action='store_true')
+    group.add_argument("-test_start", "--prbs_start", help="Start arm pcie test", action='store_true')
+    group.add_argument("-test_check", "--prbs_check", help="Check arm pcie result", action='store_true')
+    group.add_argument("-arm_prbs", "--arm_prbs", help="Run ARM PRBS", action='store_true')
 
     parser.add_argument("-slot_list", "--slot_list", help="NIC slot list", type=str, default="")
-    parser.add_argument("-wtime", "--wait_time", help="Wait time", type=int, default=30)
+    parser.add_argument("-wtime", "--wait_time", help="Wait time", type=int, default=300)
     parser.add_argument("-vmarg", "--vmarg", help="Voltage Margin", type=str, default='normal')
     parser.add_argument("-no_pc", "--no_pwr_cycle", help="Test with Power Cycle", action='store_false')
 
-    parser.add_argument("-sn", "--sn", help="Serian Number", type=str, default="SN00000000")
-    parser.add_argument("-mod", "--mod", help="L1 mode", type=str, default="hod")
+    parser.add_argument("-lpbk", "--lpbk", help="Loop Back Level", type=int, default=0)
+    parser.add_argument("-dura", "--dura", help="Duration", type=int, default=60)
+    parser.add_argument("-poly", "--poly", help="Polynomial", type=str, default="PRBS31")
+    parser.add_argument("-mode", "--mode", help="PRBS Type", type=str, default="PCIE")
 
     args = parser.parse_args()
 
-    test = arm_l1()
+    test = arm_prbs()
     slot_list = args.slot_list.split(',')
-    if args.l1_start == True:
+    if args.prbs_start == True:
         for slot in args.slot_list:
-            test.test_start(int(slot), args.wait_time, args.vmarg)
+            test.test_start(int(slot), args.vmarg, args.lpbk, args.dura, args.poly, args.mode)
         sys.exit()
 
-    if args.l1_check == True:
+    if args.prbs_check == True:
         for slot in args.slot_list:
-            test.test_check(int(slot))
+            test.test_check(int(slot), args.mode)
         sys.exit()
 
-    if args.arm_l1 == True:
-        test.arm_l1(slot_list, args.wait_time, args.vmarg, args.sn, args.mod, args.no_pwr_cycle)
+    if args.arm_prbs == True:
+        test.arm_prbs(slot_list, args.wait_time, args.vmarg, args.lpbk, args.dura, args.poly, args.mode, args.no_pwr_cycle)
         sys.exit()
