e78c42faa9a5c1e783bc8de17ef090da417361b7
David Wang <dwang@hw-srv2.pensando.io>
2022-04-27 13:41:22 -0700

    recommit this file.

--- diag/python/regression/scripts/run_ddrbist.sh
+++ diag/python/regression/scripts/run_ddrbist.sh
@@ -1,10 +1,10 @@
 # !/bin/bash
 
 # Set up environment
-echo "-----------------------------"
+echo "------------------------------------"
 echo "checking uboot permission"
 permit=$(fwenv -n gold)
-if [[ ${permit} != *"run_ddrbist_ok=1"* ]];then
+if [[ ${permit} != *"run_ddr_bist_ok=1"* ]];then
     echo "uboot setting is not allowing to run automated ddr bist"
     exit 0
 fi
@@ -18,6 +18,8 @@ if [[ ! -f /data/ddrbist_run_valid ]]; then
     exit 0
 fi
 
-echo "found the flag to run ddr bist"
+echo "found the flash to run ddr bist"
 rm /data/ddrbist_run_valid
-run_ddrbist_arm.sh
+echo "RUNNING" > /data/ddrbist_status
+/data/nic_util/run_ddrbist_arm.sh
+echo "FINISHED" > /data/ddrbist_status
