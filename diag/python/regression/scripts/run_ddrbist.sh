cb1a8b5a3a1c2c8da57ebb4432246bc461e785fa
David Wang <dwang@hw-srv2.pensando.io>
2022-04-28 17:06:35 -0700

    fixed a bug when switching consoles with multiple slots moved uboot env check after env init

--- diag/python/regression/scripts/run_ddrbist.sh
+++ diag/python/regression/scripts/run_ddrbist.sh
@@ -2,16 +2,16 @@
 
 # Set up environment
 echo "------------------------------------"
+echo "Preparing diag environment"
+source /data/nic_arm/nic_setup_env.sh
+source /etc/profile
+
 echo "checking uboot permission"
-permit=$(fwenv -n gold)
-if [[ ${permit} != *"run_ddr_bist_ok=1"* ]];then
+fwenv -n gold | grep "run_ddrbist_ok=1"
+if [ $? = 1 ];then
     echo "uboot setting is not allowing to run automated ddr bist"
     exit 0
 fi
-echo "Preparing diag environment"
-mount /dev/mmcblk0p10 /data
-source /data/nic_arm/nic_setup_env.sh
-source /etc/profile
 
 if [[ ! -f /data/ddrbist_run_valid ]]; then
     echo "no valid flag is set to run ddr bist! exit ..."
