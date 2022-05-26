0db1750149494a6bbccd8e70e35978928d376bfa
David Wang <dwang@hw-srv2.pensando.io>
2022-05-25 21:17:58 -0700

    revert the file

--- diag/scripts/asic/update_diag_asic_repo_new.sh
+++ diag/scripts/asic/update_diag_asic_repo_new.sh
@@ -8,12 +8,11 @@ ARCH=$2
 ASIC_TYPE=${ASIC_TYPE,,}
 ARCH=${ARCH,,}
 
-ASIC_REPO="/home/$USER/ws/temp/asic/"
-ASIC_TMP="/home/$USER/ws/temp/"
+ASIC_REPO="/home/$USER/workspace/asic/"
+ASIC_TMP="/home/$USER/workspace/temp/"
 
 #DIAG_REPO_TOP="/vol/hw/diag/diag_repo/asic.2020.02.10/"
-#DIAG_REPO_TOP="/vol/hw/diag/diag_repo/asic/"
-DIAG_REPO_TOP="/vol/hw/diag/diag_repo/dwang_asic/"
+DIAG_REPO_TOP="/vol/hw/diag/diag_repo/asic/"
 
 if [[ $ASIC_TYPE == "capri" ]]
 then
@@ -36,15 +35,13 @@ else
         DIAG_REPO="$DIAG_REPO_TOP/$ASIC_TYPE/$ARCH/"
         #DIAG_REPO="/vol/hw/diag/diag_repo/asic/elba/amd64/"
         #ASIC_GEN="/vol/dump/$USER/workspace/asic/ip/cosim/diag/nic/"
-        #ASIC_IMG="/vol/dump/$USER/workspace/elba_amd64/ip/cosim/diag_elb/nic.tar.gz"
-        ASIC_IMG="/vol/dump/xguo2/workspace/elba_amd64/ip/cosim/diag_elb/nic.tar.gz"
+        ASIC_IMG="/vol/dump/$USER/workspace/elba_amd64/ip/cosim/diag_elb/nic.tar.gz"
         ASIC_TMP_NIC=$ASIC_TMP/nic
     else
         DIAG_REPO="$DIAG_REPO_TOP/$ASIC_TYPE/$ARCH/"
         #DIAG_REPO="/vol/hw/diag/diag_repo/asic/elba/arm64"
         #ASIC_GEN="/vol/dump/$USER/workspace/asic/ip/cosim/diag/nic/fake_root_target/nic/"
-        #ASIC_IMG="/vol/dump/$USER/workspace/elba_arm64/ip/cosim/diag_elb/nic.tar.gz"
-        ASIC_IMG="/vol/dump/xguo2/workspace/asic/ip/cosim/diag_elb/nic.tar.gz"
+        ASIC_IMG="/vol/dump/$USER/workspace/elba_arm64/ip/cosim/diag_elb/nic.tar.gz"
         ASIC_TMP_NIC=$ASIC_TMP/nic/fake_root_target/nic/
     fi
 fi
