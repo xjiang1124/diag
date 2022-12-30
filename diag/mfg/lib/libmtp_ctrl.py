6aa2ec348ca39c7a468a14db32cafa5ee16aed04
Hongjun Fan <hongjun@pensando.io>
2022-12-30 11:08:07 -0800

    Revert "updated CPLD with ortano2 family modeling"
    This reverts commit 904cd810eaa71fc0274ec9b3c82afe1b39fb25c1.

--- diag/mfg/lib/libmtp_ctrl.py
+++ diag/mfg/lib/libmtp_ctrl.py
@@ -3217,7 +3217,7 @@ class mtp_ctrl():
                 self.cli_log_slot_err_lock(slot, "Check SWI Software Image: Software Image match to nic part number failed")
                 return False
         elif naples_pn[0:7] == "68-0077":     #ORTANO2 SOLO
-            if software_pn != "90-0020-0001":
+            if software_pn != "90-9999-0001":
                 self.cli_log_slot_err_lock(slot, "Check SWI Software Image: Software Image match to nic part number failed")
                 return False
         elif naples_pn[0:7] == "68-0049":     #ORTANO2 ADI CR
