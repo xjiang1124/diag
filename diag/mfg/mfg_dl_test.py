066c0c05ebcca81bb74bf221ace9220082f7fb96
Tony Lan <tlan@pensando.io>
2023-06-06 10:04:04 -0700

    fixed in bug
    Signed-off-by: Tony Lan <tlan@pensando.io>

--- diag/mfg/mfg_dl_test.py
+++ diag/mfg/mfg_dl_test.py
@@ -307,7 +307,7 @@ def main():
                         mtp_dl_image_list.append(NIC_IMAGES.fail_cpld_img[card_type])
                     except KeyError:
                         mtp_mgmt_ctrl.cli_log_err("mfg_cfg is missing failsafe cpld image for {:s}".format(card_type))
-                if card_type in (ELBA_NIC_TYPE_LIST, GIGLIO_NIC_TYPE_LIST) and card_type not in FPGA_TYPE_LIST:
+                if card_type in (ELBA_NIC_TYPE_LIST + GIGLIO_NIC_TYPE_LIST) and card_type not in FPGA_TYPE_LIST:
                     try:
                         mtp_dl_image_list.append(NIC_IMAGES.fea_cpld_img[card_type])
                     except KeyError:
--- diag/mfg/mfg_scan_dl_test.py
+++ diag/mfg/mfg_scan_dl_test.py
@@ -497,7 +497,7 @@ def main():
                     mtp_dl_image_list.append(NIC_IMAGES.fail_cpld_img[nic_type])
                 except KeyError:
                     mtp_mgmt_ctrl.cli_log_err("mfg_cfg is missing failsafe cpld image for {:s}".format(nic_type))
-            if nic_type in (ELBA_NIC_TYPE_LIST, GIGLIO_NIC_TYPE_LIST) and nic_type not in FPGA_TYPE_LIST:
+            if nic_type in (ELBA_NIC_TYPE_LIST + GIGLIO_NIC_TYPE_LIST) and nic_type not in FPGA_TYPE_LIST:
                 try:
                     mtp_dl_image_list.append(NIC_IMAGES.fea_cpld_img[nic_type])
                 except KeyError:
