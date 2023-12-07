71c863e41b00020eaee78b1e3fe4d3051528e37b
nabeel-0 <nabeel@pensando.io>
2023-12-06 16:59:11 -0800

    Fix column command not being available in util-linux pkg

--- asset-build/push_build.sh
+++ asset-build/push_build.sh
@@ -47,7 +47,7 @@ tar xfO ${amd64img} diag/scripts/version.txt | head > /release/${ASIC}_${RELEASE
 tar xfO ${arm64img} nic.tar.gz | tar xzO nic/fake_root_target/nic/asic_src/ip/cosim/tclsh/.git_rev.tcl > /release/${ASIC}_${RELEASE}/ASIC_VERSION
 
 echo "Collect release targets"
-grep ${ASIC} /psdiag/test/jobs.cfg | grep -v "#" | awk -F"\t" '{c=$NF;$NF=$(NF-1)=$(NF-2)=""; print c,$0}' | column -t > /release/${ASIC}_${RELEASE}/TARGETS
+grep ${ASIC} /psdiag/test/jobs.cfg | grep -v "#" | awk -F"\t" '{$(NF-1)=$(NF-2)=""; OFS="\t"; $1=$1; print}' > /release/${ASIC}_${RELEASE}/TARGETS
 
 echo "Collect release changelist"
 cp /psdiag/diag/mfg/CHANGES.md /release/${ASIC}_${RELEASE}/CHANGES.md
