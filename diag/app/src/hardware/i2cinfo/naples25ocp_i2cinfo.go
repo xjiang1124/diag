040c48bb56f6db2e45e4fcbcd96211e306349220
Xin Guo <xin.guo@pensando.io>
2024-04-01 15:49:51 -0700

    Update SWM and OCP FRU I2C table

--- diag/app/src/hardware/i2cinfo/naples25ocp_i2cinfo.go
+++ diag/app/src/hardware/i2cinfo/naples25ocp_i2cinfo.go
@@ -5,8 +5,8 @@ var Naples25OcpMtpTbl = []I2cInfo {
     //       name              comp         Bus    devAddr  channel HubName   HubPort  Flag
     I2cInfo {"CPLD",           "CPLD",      0x0,   0x4A,    0x0,    "HUB_NONE",  0,    0},
     I2cInfo {"CPLD_ADAP",      "CPLD",      0x0,   0x4B,    0x0,    "HUB_NONE",  0,    0},
-    I2cInfo {"FRU",            "AT24C02C",  0x0,   0x50,    0x0,    "HUB_NONE",  0,    Naples25OcpMtpTbl},
-    I2cInfo {"FRU_ADAP",       "AT24C02C",  0x0,   0x57,    0x0,    "HUB_NONE",  0,    Naples25OcpMtpTbl},
-    I2cInfo {"FRU_SERIALNUM",  "AT24C02C",  0x0,   0x58,    0x0,    "HUB_NONE",  0,    Naples25OcpMtpTbl},
+    I2cInfo {"FRU",            "AT24C02C",  0x0,   0x50,    0x0,    "HUB_NONE",  0,    FLAG_16BIT_EEPROM},
+    I2cInfo {"FRU_ADAP",       "AT24C02C",  0x0,   0x57,    0x0,    "HUB_NONE",  0,    0},
+    I2cInfo {"FRU_SERIALNUM",  "AT24C02C",  0x0,   0x58,    0x0,    "HUB_NONE",  0,    0},
 }
 
