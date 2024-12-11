929fb6e58546eb7317bb454e972e3152b660e36b
Yanmin Hua <yanmin@pensando.io>
2024-12-11 15:08:42 -0800

    store SPI mode on/off status in a shared file

--- diag/app/src/device/fpga/materafpga/materafpga.go
+++ diag/app/src/device/fpga/materafpga/materafpga.go
@@ -544,6 +544,7 @@ func SetJTAGbusToSPI(slot uint32) (err error) {
     readArray, err_r := readArrayFromFile(fpga_misc_ctrl_reg_filename)
     if err_r == nil {
         if len(readArray) == 10 {
+            jtag_bus_spi = readArray
             if readArray[slot] == 1 {
                 fpga_misc_ctrl_reg_lock.Unlock()
                 return
@@ -597,6 +598,7 @@ func SetJTAGbusToJTAG(slot uint32) (err error) {
     readArray, err_r := readArrayFromFile(fpga_misc_ctrl_reg_filename)
     if err_r == nil {
         if len(readArray) == 10 {
+            jtag_bus_spi = readArray
             if readArray[slot] == 0 {
                 fpga_misc_ctrl_reg_lock.Unlock()
                 return
