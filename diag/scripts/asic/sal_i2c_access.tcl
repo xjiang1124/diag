044b5e1f64e0bf01ec84433a15b2c8f172161e14
Yanmin Hua <yanmin@pensando.io>
2025-01-03 13:55:46 -0800

    Move j2c clear before opening j2c in i2c access test

--- diag/scripts/asic/sal_i2c_access.tcl
+++ diag/scripts/asic/sal_i2c_access.tcl
@@ -1,5 +1,6 @@
+# !/usr/bin/tclsh
 source /home/diag/diag/scripts/asic/cmdline.tcl
-source /home/diag/diag/scripts/asic/sal_diag_utils.tcl
+
 set usage {
     {slot.arg       ""          "Slot number"}
     {vmarg.arg      "none"      "Voltage margin"}
@@ -8,52 +9,7 @@ set usage {
 array set arg [cmdline::getoptions argv $usage]
 foreach argname [array names arg] { set $argname $arg($argname) }
 if { $slot == "" } { puts "Missing required --slot arg" ; exit -1 }
-
-set ASIC_SRC $::env(ASIC_SRC)
-cd $ASIC_SRC/ip/cosim/tclsh
-source .tclrc.diag.sal
-source /home/diag/diag/scripts/asic/sal_diag_utils.tcl
-
-set ::FAN_SPD 80
-set ::DEVMGR devmgr_v2
-
-set port $slot
-set slot $slot
-set ::slot  $slot
-set ::port  $port
-diag_close_j2c_if $port $slot
-diag_open_j2c_if $port $slot
-diag_close_j2c_if $port $slot
-
-after 500
-diag_close_ow_if $port $slot
-after 500
-diag_open_ow_if $port $slot
-after 2000
-sal_ow_axi
-
-set dd 0
-set cnt 0
-while { ($dd==0) && ($cnt<10) } {
-    csr_write sal0.ms.ms.cfg_ow 3
-    after 500
-    set dd [ rd sal0.ms.ms.cfg_ow ]
-    incr cnt
-}
-set ret 1
-if { $cnt  >= 10 } {
-    plog_err "\n\n==== J2C / OW is not working.... Ping HW team\n\n"
-    return
-}
-sal_j2c
-set j2c_secure 1
-#sal_ow
-
-set val [_msrd]
-if { $val != 0x1 } {
-    plog_msg "OW sanity test failed!"
-    exit 0
-}
+parray arg
 
 proc sal_rtc_access_test {} {
     set secondPre [sal_smbus_read_byte_data 2 0x51 1]
@@ -90,25 +46,38 @@ proc sal_rc19004_access_test {} {
     }
 }
 
+source /home/diag/diag/scripts/asic/sal_diag_utils.tcl
+
+set ASIC_SRC $::env(ASIC_SRC)
+cd $ASIC_SRC/ip/cosim/tclsh
+source .tclrc.diag.sal
+
+set slot $slot
+set port $slot
+set ::slot $slot
+set ::port $port
+
 # start logfile
 set cur_time [clock format [clock seconds] -format %m%d%y_%H%M%S]
 set log_file $ASIC_SRC/ip/cosim/tclsh/sal_i2c_access_SLOT${slot}_${cur_time}.log
 plog_stop
 plog_start $log_file 1000000000
+
+plog_msg "Opening j2c"
 set err_cnt_init [ plog_get_err_count ]
-
-exec fpgautil spimode $slot off
 exec jtag_accpcie_salina clr $slot
+exec fpgautil spimode $slot off
+sal_j2c
+plog_msg "_msrd"
+plog_msg [eval _msrd]
 reset_to_proto_mode
-
-# set vmarg
 sal_set_vmarg $vmarg
 
 # run test
 sal_rtc_access_test
 sal_rc19008_access_test
 sal_rc19004_access_test
-diag_close_j2c_if $::slot $::port
+diag_close_j2c_if $port $slot
 
 # close
 plog_stop
