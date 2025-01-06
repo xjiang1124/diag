b57fb6108c2bab71ae736cc9cf5f2c8e47c078af
nabeel-0 <nabeel@pensando.io>
2025-01-06 09:53:04 -0800

    One more fix for leni mbist

--- diag/scripts/asic/sal_diag_utils.tcl
+++ diag/scripts/asic/sal_diag_utils.tcl
@@ -63,6 +63,11 @@ proc set_pollara_frequency_2000_workaround {} {
 }
 
 proc verify_arm_frequency {{arm_freq "1500"}} {
+    set card_type [sal_get_card_type]
+    if { $card_type != "POLLARA" } {
+        # not supported
+        return
+    }
     sal_PLL_SYSPLL_T_ARM_PLL_DFX_CLK_OBS $arm_freq
 }
 
