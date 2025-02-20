081350c141ee52d3620b6305411162918c8c3e2f
Xin Guo <xin.guo@pensando.io>
2025-02-20 14:04:48 -0800

    Added more comments to sal_esecure_lockbits.sh

--- diag/scripts/asic/sal_esecure_lockbits.sh
+++ diag/scripts/asic/sal_esecure_lockbits.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # param 1: slot
-# param 2: value at 0x400
-# param 3: value at 0x401
+# param 2: value at 0x400; use 0xbd to enable esecure; program other value to disable
+# param 3: value at 0x401; use 0xbd to enable hw lock; program other value to disable
 echo "Esecure HW Lock Bits --> Slot-$1 0x400=$2 0x401=$3"
 fpgautil cpld $1 generate ufm2 ufm2.bin
 printf "$(printf '\\x%02X' $2)" | dd of=ufm2.bin bs=1 seek=1024 count=1 conv=notrunc &> /dev/null
