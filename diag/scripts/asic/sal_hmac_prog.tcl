3404f11ff8317843f1c5c2eb27147cafb52a46ac
dwang <you@example.com>
2025-03-19 15:23:18 -0700

    added code to burn HMAC fuse and protection bits

--- diag/scripts/asic/sal_hmac_prog.tcl
+++ diag/scripts/asic/sal_hmac_prog.tcl
@@ -55,98 +55,111 @@ puts $rtn
 reset_to_proto_mode cold
 sal_print_voltage_temp_from_j2c
 
+set bit_lock 0
+set hmac_done 0
+
 # Check if efuse[47:46] is already set
 set bit46 [sal_fuse_get_bit 0 46 0]
 set bit47 [sal_fuse_get_bit 0 47 0]
 if { $bit46 != 0 || $bit47 != 0 } {
-    plog_msg "Efuse HAM already been programmed, exit here"
+    plog_msg "Efuse HMAC protection bits already been programmed"
+    set bit_lock 1
     sal_fuse_dump
     plog_msg "EFUSE PROG PASSED"
     diag_close_j2c_if $port $slot
     plog_stop
     return 0
-} else {
-    set hmac_sum 0
-    for {set i 0} {$i < 16} {incr i} {
-        set idx [expr $i + 16]
-        set fuse_val [sal_fuse_get_line 0 $idx 0]
-        set hmac_sum [expr {$fuse_val | $hmac_sum}]
-    }
-    if { $hmac_sum != 0 } {
-        plog_msg "HMAC is programmed but read protection is not"
-        sal_fuse_dump
-        plog_msg "EFUSE PROG FAILED"
-        diag_close_j2c_if $port $slot
-        plog_stop
-        return 0
-    }
+} 
+
+set hmac_sum 0
+for {set i 0} {$i < 16} {incr i} {
+    set idx [expr $i + 16]
+    set fuse_val [sal_fuse_get_line 0 $idx 0]
+    set hmac_sum [expr {$fuse_val | $hmac_sum}]
+}
+if { $hmac_sum != 0 } {
+    plog_msg "HMAC is programmed"
+    set hmac_done 1
 }
 
 set in_err [plog_get_err_count]
 set fp [open $hmac_file r]
 set hmac_str [read $fp]
 close $fp
+plog_msg $hmac_str
  
 set str_start 0
 set str_end 7
 set len [string length $hmac_str]
 
-for { set i 0 } { $str_start < $len } { incr i } {
-    lappend list_result [ string range $hmac_str $str_start $str_end ]
-    incr str_start 8
-    incr str_end 8
-}
-
-set i 0
-foreach n $list_result {
-    set hmac_val($i) "0x$n"
-    incr i 1
-}
-
-sal_fuse_vddq_enable
-set idx 16 
-for {set i 0} {$i < 16 } {incr i} {
-    plog_msg "set fuse line $idx with value $hmac_val($i)"
-    if { $dry_run  == 0 } {
-        sal_fuse_set_line 0 $idx $hmac_val($i)
+if { $hmac_done == 0 } {
+    for { set i 0 } { $str_start < $len } { incr i } {
+        lappend list_result [ string range $hmac_str $str_start $str_end ]
+        incr str_start 8
+        incr str_end 8
     }
-    incr idx 1
-}
-sal_fuse_vddq_disable
 
-sal_fuse_dump
-
-for {set i 0} {$i < 16} {incr i} {
-    set idx [expr $i + 16]
-    set fuse_read($i) [sal_fuse_get_line 0 $idx 0]
-    if { "$fuse_read($i)" != "$hmac_val($i)" } {
-        plog_msg "failed to set fuse value read: $fuse_read($i) expected: $hmac_val($i)"
-        sal_fuse_dump
-        plog_msg "EFUSE PROG FAILED"
-        diag_close_j2c_if $port $slot
-        plog_stop
-        return
+    set i 0
+    foreach n $list_result {
+        set hmac 0
+        set hmac_1 [string range $n 0 1]
+        set hmac_2 [string range $n 2 3]
+        set hmac_3 [string range $n 4 5]
+        set hmac_4 [string range $n 6 7]
+        set hmac [concat $hmac_4$hmac_3$hmac_2$hmac_1]
+        set hmac_val($i) "0x$hmac"
+        incr i 1
     }
-}
 
-plog_msg "set read disable bit 22"
-plog_msg "set read disable bit 23"
-if { $dry_run  == 0 } {
     sal_fuse_vddq_enable
-    sal_fuse_set_bit 0 46 0 
-    sal_fuse_set_bit 0 47 0 
+    set idx 16 
+    for {set i 0} {$i < 16 } {incr i} {
+        plog_msg "set fuse line $idx with value $hmac_val($i)"
+        if { $dry_run  == 0 } {
+            sal_fuse_set_line 0 $idx $hmac_val($i)
+        }
+        incr idx 1
+    }
     sal_fuse_vddq_disable
+    if { $dry_run == 0 } {
+        for {set i 0} {$i < 16} {incr i} {
+            set idx [expr $i + 16]
+            set fuse_read($i) [sal_fuse_get_line 0 $idx 0]
+            if { "$fuse_read($i)" != "$hmac_val($i)" } {
+                plog_msg "fuse program varification failed: $fuse_read($i) expected: $hmac_val($i)"
+                sal_fuse_dump
+                plog_msg "EFUSE PROG FAILED"
+                diag_close_j2c_if $port $slot
+                plog_stop
+                return
+            }
+        }
+    }
+    sal_fuse_dump
 }
 
-set bit46 [sal_fuse_get_bit 0 46 0]
-set bit47 [sal_fuse_get_bit 0 43 0]
-if { $bit46 != 0 || $bit47 != 0 } {
-    plog_msg "failed to set read disable bits $bit46 $bit47"
+
+if { $bit_lock == 0 } {
+    plog_msg "set read disable bit 46"
+    plog_msg "set read disable bit 47"
+    if { $dry_run  == 0 } {
+        sal_fuse_vddq_enable
+        sal_fuse_set_bit 0 46 0 
+        sal_fuse_set_bit 0 47 0 
+        sal_fuse_vddq_disable
+
+        set bit46 [sal_fuse_get_bit 0 46 0]
+        set bit47 [sal_fuse_get_bit 0 43 0]
+        if { $bit46 == 0 || $bit47 == 0 } {
+            plog_msg "failed to set hmac lock bits $bit46 $bit47"
+            sal_fuse_dump
+            plog_msg "EFUSE PROG FAILED"
+            diag_close_j2c_if $port $slot
+            plog_stop
+            return
+        }
+    }
     sal_fuse_dump
-    plog_msg "EFUSE PROG FAILED"
-    diag_close_j2c_if $port $slot
-    plog_stop
-    return
 }
 
 set out_err [plog_get_err_count]
--- diag/scripts/check_python.sh
+++ diag/scripts/check_python.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/bash
 
+need_update=0
 if test -d "/usr/local/lib/python2.7/dist-packages/cryptography-2.2.2.dist-info"; then
-    echo "MTP is read for SWI"
-    exit 0
+    echo "MTP has right cryptography package installed"
+else
+    need_update=1
 fi
-echo "MTP is NOT read for SWI"
-exit 1
+
+if test -d "/usr/local/lib/python2.7/dist-packages/asn1crypto"; then
+    echo "MTP has asn1crypto package installed"
+else
+    need_update=1
+fi
+
+if [ $need_update -eq 0 ]; then
+    echo "MTP is ready for SWI"
+    exit 0
+else
+    echo "MTP is NOT ready for SWI"
+    exit 1
+fi
