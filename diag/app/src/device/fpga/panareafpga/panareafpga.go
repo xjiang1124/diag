b0039e47a5a90fe676b3b4f48e69366512cc7887
andrew-dieckman1 <adieckman@pensando.io>
2025-07-30 16:29:02 -0700

    Change how the mtp i/o expander switches the shared spi/j2c bus from spi to j2c.    Now the output will be left enable the the GPIO signal will be driven low. Add initial support for Panarea MTP for fpga read/write.

--- diag/app/src/device/fpga/panareafpga/panareafpga.go
+++ diag/app/src/device/fpga/panareafpga/panareafpga.go
@@ -1,10 +1,130 @@
 package panareafpga
 import (
-    //"errors"
+    "bufio"
     "fmt"
-    //"common/errType"
+    "os"
+    "os/exec"
+    "strconv"
+    "strings"
+    "syscall"
+    "unsafe"
+    "common/cli"
     "device/fpga/materafpga"
 )
+
+
+var Glob_fd0 *os.File = nil
+var Glob_mmap0 []byte
+
+const MAP_SIZE int = 1048576
+
+
+func init () {
+    var bar uint64 =0
+    exists, _ := Path_exists("/tmp/fpgabars")
+
+    //TRY TO STORE THE BAR VALUES IN A FILE.  WE DONT WANT TO SCAN THE PCI AND GET THE BARS EVERYTIME WE USE ONE OF THE DIAG UTILITIES THAT CALLS THIS INIT
+    if exists == true {
+        file, errGo := os.Open("/tmp/fpgabars")
+        if errGo != nil {
+            cli.Println("e", "ERROR: Failed to get FPGA BAR VALUE.   GO ERROR=%v", errGo)
+            return
+        }
+        scanner := bufio.NewScanner(file)
+        for scanner.Scan() {
+            line := scanner.Text()
+            bar, _ = strconv.ParseUint(strings.TrimSuffix(line, "\n"), 0, 64)
+        }
+        file.Close()
+    } else {
+        shcmds := []string{ "lspci -v -d 1dd8:000D | grep 'Memory at' | awk '{print $3}'"}
+        execOutput, errGo := exec.Command("sh", "-c", shcmds[0] ).Output()
+        if errGo != nil {
+            cli.Println("e", "ERROR: Failed to get FPGA BAR VALUE.   GO ERROR=%v", errGo)
+            return
+        }
+        tmp := "0x" + strings.TrimSuffix(string(execOutput), "\n")
+        bar, _ = strconv.ParseUint(tmp, 0, 64) 
+
+        file, errGo := os.OpenFile("/tmp/fpgabars", os.O_RDWR | os.O_CREATE | os.O_TRUNC, 0644)
+        if errGo != nil {
+            cli.Println("e", "ERROR: Failed to get open file /tmp/fpgabars.   GO ERROR=%v", errGo)
+            return
+        }
+        datawriter := bufio.NewWriter(file)
+        _, _ = datawriter.WriteString(fmt.Sprintf("0x%x\n", bar))
+ 
+        datawriter.Flush()
+        file.Close() 
+         
+    }
+    Glob_mmap0, Glob_fd0, _ = MMAP_Device(int64(bar), MAP_SIZE)
+}
+ 
+ 
+// exists returns whether the given file or directory exists
+func Path_exists(path string) (bool, error) {
+    _, err := os.Stat(path)
+    if err == nil { return true, nil }
+    if os.IsNotExist(err) { return false, nil }
+    return false, err
+}
+
+
+func MMAP_Device(addr int64, size int) (mmap []byte, fd *os.File, err error) {
+    fd, err = os.OpenFile("/dev/mem", os.O_RDWR, 0333)
+    if err != nil {
+        cli.Printf("e", "os.Open /dev/mem failed.  Err !=nil:   ERR = '%s'\n", err) 
+        return 
+    }
+
+    mmap, err = syscall.Mmap(int(fd.Fd()), addr, size, syscall.PROT_READ | syscall.PROT_WRITE, syscall.MAP_SHARED)
+    fd.Close()
+    if err != nil {
+        cli.Printf("e", "syscall.Mmap failed.  Err !=nil:   ERR = '%s'\n", err)  
+        fd.Close()
+        return 
+    }
+   
+    return 
+} 
+ 
+
+func MateraReadU8(addr uint64) (value uint8, err error) {
+    value = *(*uint8)(unsafe.Pointer(&Glob_mmap0[addr]))
+    return
+}
+
+
+func MateraReadU16(addr uint64) (value uint16, err error) {
+    value = *(*uint16)(unsafe.Pointer(&Glob_mmap0[addr]))
+    return
+}
+
+
+func MateraReadU32(addr uint64) (value uint32, err error) {
+    value = *(*uint32)(unsafe.Pointer(&Glob_mmap0[addr]))
+    return
+}
+
+
+func MateraWriteU8(addr uint64, data uint8) (err error) {
+    *(*uint8)(unsafe.Pointer(&Glob_mmap0[addr])) = data
+    return
+}
+
+
+func MateraWriteU16(addr uint64, data uint16) (err error) {
+    *(*uint16)(unsafe.Pointer(&Glob_mmap0[addr])) = data
+    return
+}
+
+
+func MateraWriteU32(addr uint64, data uint32) (err error) {
+    *(*uint32)(unsafe.Pointer(&Glob_mmap0[addr])) = data
+    return
+}
+
 
 func FpgaDumpRegionRegisters() (err error) {
 
--- diag/app/src/util/fpgautil/fpgautil.go
+++ diag/app/src/util/fpgautil/fpgautil.go
@@ -9,21 +9,25 @@ var cardType string
 
 func init() {
     cardType = os.Getenv("CARD_TYPE")
-
     if cardType != "LIPARI" && 
        cardType != "MTP_MATERA" &&
        cardType != "MTP_PANAREA" &&
        cardType != "TAORMINA" {
         //See if we are SONIC OS.. if yes we are lipari
         exists, _ := Path_exists("/etc/sonic")
+        exists_matera, _ := Path_exists("/boot/efi/EFI/ubuntu/grub.cfg")
         if exists == true {
             cardType = "LIPARI"
+            os.Setenv("CARD_TYPE",cardType)
+            cli.Println("d", "Found card:", cardType)
+        } else if exists_matera == true {
+            cardType = "MTP_MATERA"
             os.Setenv("CARD_TYPE",cardType)
             cli.Println("d", "Found card:", cardType)
         } else {
             cardType = "TAORMINA"
             os.Setenv("CARD_TYPE",cardType)
-            cli.Println("d", "Found card:", cardType)
+            cli.Println("d", "NO CARD TYPE SET.  DEFAULTING TO TAORMINA:", cardType)
         }
     }
 }
