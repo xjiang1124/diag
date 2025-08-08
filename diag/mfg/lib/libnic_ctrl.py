202d41f37ef0ab047ac14d1b51a596b1866df30b
Hongjun Fan <hongjun@pensando.io>
2025-08-08 10:49:25 -0700

    official added ALGO22 test in, and fail out if fail

--- diag/mfg/lib/libnic_ctrl.py
+++ diag/mfg/lib/libnic_ctrl.py
@@ -858,6 +858,14 @@ class nic_ctrl():
             cmd = "turn_on_slot.sh off {:s}; sleep 10; turn_on_slot.sh on {:s}".format(str(self._slot+1), str(self._slot+1))
             self.mtp_exec_cmd(cmd, timeout=MTP_Const.MTP_OS_CMD_DELAY)
 
+        # just run ALGO22 jtag mbist with cold
+        cmd = "turn_on_slot.sh off {:s}; sleep 10; turn_on_slot.sh on {:s}".format(str(self._slot+1), str(self._slot+1))
+        self.mtp_exec_cmd(cmd, timeout=MTP_Const.MTP_OS_CMD_DELAY)
+        cmd = MFG_DIAG_CMDS().MATERA_MTP_SALINA_NIC_JTAG_MBIST_WITH_TEST_LIST.format(self._sn, str(self._slot+1), vmarg, "cold", "ALGO22")
+        if not self.mtp_exec_cmd(cmd, timeout=MTP_Const.NIC_CON_CMD_DELAY):
+            return False
+        if MFG_DIAG_SIG.MATERA_NIC_SALINA_JTAG_MBIST_SIG not in self.nic_get_cmd_buf():
+            return False
         return True
 
     def nic_snake_mtp_salina(self, snake_type='esam_pktgen_max_power_sor', vmarg="normal", dura=120, timeout=3600, slot_asic_dir_path=None, ite='1', int_lpbk='0'):
