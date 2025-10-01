378854fabea79ede0264ae22bbef367891bae2ed
Yanmin Hua <yanmin@pensando.io>
2025-10-01 14:31:50 -0700

    Restore "taskset -c" for Matera uart
    - and add support for Vulcano asis lib in start_diag.sh

--- diag/scripts/con_connect.sh
+++ diag/scripts/con_connect.sh
@@ -25,17 +25,20 @@ then
         echo "uart_id=$uart_id"
     fi
     echo "UART connected to slot $1"
-    board_type=$(i2cget -y $(($slot + 2)) 0x4a 0x80)
-    if [[ "$board_type" -ge 0x62 ]]
-    then
-        data=$(i2cget -y $(($slot + 2)) 0x4a 0x21)
-        data=$(( $data & 0xF8 ))
-        data=$(( $data | $uart_id ))
-        i2cset -y $(($slot + 2)) 0x4a 0x21 $data
+    if [[ $CARD_TYPE == "MTP_MATERA" ]]
+        then
+        board_type=$(i2cget -y $(($slot + 2)) 0x4a 0x80)
+        if [[ "$board_type" -ge 0x62 ]]
+        then
+            data=$(i2cget -y $(($slot + 2)) 0x4a 0x21)
+            data=$(( $data & 0xF8 ))
+            data=$(( $data | $uart_id ))
+            i2cset -y $(($slot + 2)) 0x4a 0x21 $data
+        fi
     fi
     if [[ $CARD_TYPE == "MTP_MATERA" ]]
     then
-        fpga_uart $((slot - 1))
+        taskset -c $slot fpga_uart $((slot - 1))
     else
         fpga_uart_panarea $((slot - 1))
     fi
