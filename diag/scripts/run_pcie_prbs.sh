ee4d1430048c1b3b76ed5337b65840563a1b1ff7
Xin Guo <xin.guo@pensando.io>
2025-12-09 15:32:34 -0800

    Improve prbs test script

--- diag/scripts/run_pcie_prbs.sh
+++ diag/scripts/run_pcie_prbs.sh
@@ -26,16 +26,25 @@ nic_log=/home/diag/xin/pcie_prbs_slot${slot}_${sn}_${vmarg}.log
 
 echo "pcie_prbs_slot${slot}_${sn}_${vmarg}"  | tee $nic_log
 
+
+cd ~/diag/scripts/asic/
+
 for ite in $(seq 1 $num_ite)
 do
 
     echo "===== Ite: $ite =====" | tee -a $nic_log
-    tclsh /home/diag/xin/sal_pcie_aw_ber_collect.pollara.tcl -slot $slot -card_type $card_type -dura 600 -aw_txfir_ow 9 -cap_ffe 0| tee -a $nic_log
+    turn_on_slot.sh off $slot
+    sleep 5
+    turn_on_slot.sh on $slot
+    sleep 15
+
+    #tclsh /home/diag/xin/sal_pcie_aw_ber_collect.pollara_v2.tcl -slot $slot -card_type $card_type -dura 600 -aw_txfir_ow 9 -cap_ffe 0| tee -a $nic_log
+    tclsh sal_pcie_prbs.pollara_v2.tcl -slot $slot -card_type $card_type -dura 60 | tee -a $nic_log
     inventory -sts -slot $slot | tee -a $nic_log
 
     if [[ $stop_on_err == "1" ]]
     then
-        num_err=$(grep "ERROR :" $nic_log | wc | awk -F " " '{print $1}')
+        num_err=$(grep -a "ERROR :" $nic_log | wc | awk -F " " '{print $1}')
         if [[ $num_err -ne 0 ]]
         then
             echo "Error has happened, exiting"
